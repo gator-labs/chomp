ce8cfa8c2553095170dc700ea2a42cf14e6ec0a3
avuletic01 <avuletic@profico.com>
2024-03-19 21:00:28 +0100

    Add switch navigation component

---
+++ app/components/SwitchNavigation/SwitchNavigation.tsx
@@ -0,0 +1,26 @@
+import Link from "next/link";
+import { Button } from "../Button/Button";
+import classNames from "classnames";
+
+type SwitchNavigationProps = {
+  navigationItems: { href: string; label: string; isActive: boolean }[];
+};
+
+export function SwitchNavigation({ navigationItems }: SwitchNavigationProps) {
+  return (
+    <div className="bg-black flex rounded-full p-1 space-x-2 w-full">
+      {navigationItems.map((ni, index) => (
+        <Link key={index} href={ni.href} className="w-1/2">
+          <Button
+            className={classNames("!rounded-full", {
+              "text-gray !border-gray": !ni.isActive,
+            })}
+            variant={ni.isActive ? "white" : "secondary"}
+          >
+            {ni.label}
+          </Button>
+        </Link>
+      ))}
+    </div>
+  );
+}
