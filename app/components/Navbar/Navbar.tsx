d8d9ae75c6b87699d414a829e3a9c8dca5f4a168
avuletic01 <avuletic@profico.com>
2024-03-20 18:19:21 +0100

    Add navbar

---
+++ app/components/Navbar/Navbar.tsx
@@ -0,0 +1,32 @@
+import { ReactNode } from "react";
+import { Avatar } from "../Avatar/Avatar";
+import { WalletIcon } from "../Icons/WalletIcon";
+import Link from "next/link";
+
+type NavbarProps = {
+  children?: ReactNode;
+  avatarSrc: string;
+  walletLink: string;
+  avatarLink: string;
+};
+
+export function Navbar({
+  children,
+  avatarSrc,
+  walletLink,
+  avatarLink,
+}: NavbarProps) {
+  return (
+    <nav className="bg-btn-text-primary flex justify-between w-full px-4 py-3 items-center">
+      <div>{children}</div>
+      <div className="flex gap-2 items-center">
+        <Link href={walletLink}>
+          <WalletIcon />
+        </Link>
+        <Link href={avatarLink}>
+          <Avatar src={avatarSrc} size="small" />
+        </Link>
+      </div>
+    </nav>
+  );
+}
