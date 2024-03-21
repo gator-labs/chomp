7cbcf334af7501115304d27661f048d76957f0cc
Bartol Deak <b@bdeak.net>
2024-03-21 12:12:42 +0100

    wrap token balance in suspense

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,17 @@
 import { LogoutButton } from "./components/LogoutButton/LogoutButton";
 import { PageLayout } from "./components/PageLayout/PageLayout";
-import { getUserTokenBalances } from "../lib/web3";
 import { HomeSwitchNavigation } from "./components/HomeSwitchNavigation/HomeSwitchNavigation";
+import { Suspense } from "react";
+import { UserTokenBalance } from "./components/UserTokenBalance/UserTokenBalance";
 
 export default async function Page() {
-  const { bonk } = await getUserTokenBalances();
-
   return (
     <PageLayout>
       <HomeSwitchNavigation />
       <div>home page</div>
-      <div>bonk balance: {bonk}</div>
+      <Suspense fallback={<div>loading bonk balance...</div>}>
+        <UserTokenBalance />
+      </Suspense>
       <LogoutButton />
     </PageLayout>
   );
