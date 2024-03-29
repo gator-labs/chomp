7f88c1b453a2589834f610ffa9fdd86c463d1ba1
Bartol Deak <b@bdeak.net>
2024-03-29 18:49:03 +0100

    pwa support wip

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
+import withPWA from "next-pwa";
+
+/** @type {import('next-pwa').PWAConfig} */
+const pwaConfig = {};
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default nextConfig;
+export default withPWA(pwaConfig)(nextConfig);
