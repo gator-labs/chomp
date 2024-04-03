74335a2e4ff883f52cef671198093315e5d961db
Bartol Deak <b@bdeak.net>
2024-04-03 14:39:19 +0200

    answer page

--- app/application/answer/page.tsx
+++ app/application/answer/page.tsx
@@ -1,3 +1,10 @@
-export default function Page() {
-  return <>Answer page</>;
+import { getUnansweredDailyQuestions } from "@/app/queries/question";
+import { shuffleArray } from "@/app/utils/randomUtils";
+
+export default async function Page() {
+  const unansweredQuestions = await getUnansweredDailyQuestions();
+
+  const [randomQuestion] = shuffleArray(unansweredQuestions);
+
+  return <div>Answer2 page {unansweredQuestions.length}</div>;
 }
