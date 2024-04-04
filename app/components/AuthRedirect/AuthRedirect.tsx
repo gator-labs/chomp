aec328b04b41e925d2909105e81cd6377fbc777c
Bartol Deak <b@bdeak.net>
2024-04-04 19:18:06 +0200

    login next url and add frame endpoint

--- app/components/AuthRedirect/AuthRedirect.tsx
+++ app/components/AuthRedirect/AuthRedirect.tsx
@@ -1,11 +1,16 @@
 import { redirect } from "next/navigation";
 import { getJwtPayload } from "@/app/actions/jwt";
+import { headers } from "next/headers";
 
 export const AuthRedirect = async () => {
   const jwt = await getJwtPayload();
+  const path = headers().get("x-path");
 
   if (!jwt) {
-    redirect("/login");
+    if (!path || path === "/application") {
+      redirect("/login");
+    }
+    redirect(`/login?next=${encodeURIComponent(path)}`);
   }
 
   return null;
--- next.config.js
+++ next.config.js
@@ -1,16 +1,16 @@
 // Configuration options for Next.js
 const nextConfig = {
-  reactStrictMode: true, // Enable React strict mode for improved error handling
+  reactStrictMode: false, // Enable React strict mode for improved error handling
   swcMinify: true, // Enable SWC minification for improved performance
   compiler: {
-    removeConsole: process.env.NODE_ENV !== 'development', // Remove console.log in production
+    removeConsole: process.env.NODE_ENV !== "development", // Remove console.log in production
   },
 };
 
 // Configuration object tells the next-pwa plugin
-const withPWA = require('next-pwa')({
-  dest: 'public', // Destination directory for the PWA files
-  disable: process.env.NODE_ENV === 'development', // Disable PWA in development mode
+const withPWA = require("next-pwa")({
+  dest: "public", // Destination directory for the PWA files
+  disable: process.env.NODE_ENV === "development", // Disable PWA in development mode
   register: true, // Register the PWA service worker
   skipWaiting: true, // Skip waiting for service worker activation
 });
