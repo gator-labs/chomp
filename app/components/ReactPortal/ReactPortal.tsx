3dd0c6a10fddda1e79196280710869e12f402e5f
avuletic01 <avuletic@profico.com>
2024-04-05 07:19:18 +0200

    Add modal component

---
+++ app/components/ReactPortal/ReactPortal.tsx
@@ -0,0 +1,44 @@
+import { useState, useLayoutEffect, ReactNode } from "react";
+import { createPortal } from "react-dom";
+
+type ReactPortalProps = {
+  children: ReactNode;
+  wrapperId?: string;
+};
+
+function createWrapperAndAppendToBody(wrapperId: string) {
+  const wrapperElement = document.createElement("div");
+  wrapperElement.setAttribute("id", wrapperId);
+  document.body.appendChild(wrapperElement);
+  return wrapperElement;
+}
+
+export function ReactPortal({
+  children,
+  wrapperId = "react-portal-wrapper",
+}: ReactPortalProps) {
+  const [wrapperElement, setWrapperElement] = useState<HTMLElement | null>(
+    null
+  );
+
+  useLayoutEffect(() => {
+    let element = document.getElementById(wrapperId);
+    let isCreatedInComponent = false;
+    if (!element) {
+      isCreatedInComponent = true;
+      element = createWrapperAndAppendToBody(wrapperId);
+    }
+
+    setWrapperElement(element);
+
+    return () => {
+      if (isCreatedInComponent && element.parentNode) {
+        element.parentNode.removeChild(element);
+      }
+    };
+  }, [wrapperId]);
+
+  if (wrapperElement === null) return null;
+
+  return createPortal(children, wrapperElement);
+}
---
+++ stories/Modal.stories.tsx
@@ -0,0 +1,47 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { Modal } from "../app/components/Modal/Modal";
+import { Button } from "@/app/components/Button/Button";
+import { fn } from "@storybook/test";
+
+const meta = {
+  title: "Modal",
+  component: Modal,
+  parameters: {
+    layout: "centered",
+    controls: {
+      exclude: ["children"],
+    },
+  },
+  tags: ["autodocs"],
+  args: {
+    isOpen: true,
+    onClose: fn(),
+  },
+  decorators: (Story) => (
+    <div className="w-52 h-[512px]">
+      <Story />
+    </div>
+  ),
+} satisfies Meta<typeof Modal>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    children: (
+      <div className="text-sm text-white font-sora">
+        <div className="mb-4">
+          This is a generic modal with a close button on the top right
+        </div>
+        <div>
+          <Button variant="white">Button</Button>
+        </div>
+        <div className="mt-2">
+          <Button variant="black">Button</Button>
+        </div>
+      </div>
+    ),
+    title: "Title",
+  },
+};
