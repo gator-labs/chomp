ac7aa89213633f3e7dfc1eaab12b8854e98f6c98
Danijel Radomiljac <danijel.radomiljac@blankhq.co>
2024-04-13 23:44:12 +0200

    add tutorial page

---
+++ app/tutorial/constants.tsx
@@ -0,0 +1,48 @@
+export const TUTORIAL_STEPS = [
+  {
+    text: (
+      <p className="text-center">
+        The name of the game is to answer as many questions as accurately as you
+        can.
+      </p>
+    ),
+  },
+  {
+    text: (
+      <>
+        <p>There are 2 types of questions.</p>
+        <p className="text-pink">True or False</p>
+        <p>and</p>
+        <p className="text-pink">Multiple Choice</p>
+      </>
+    ),
+  },
+
+  {
+    text: (
+      <>
+        <p>For each question, you will be asked two different things.</p>
+        <p>
+          What do <span className="text-pink">you</span> think is the answer.
+        </p>
+        <p>and</p>
+        <p>
+          What do you think <span className="text-pink">others</span> think is
+          the answer.
+        </p>
+      </>
+    ),
+  },
+  {
+    text: (
+      <>
+        <p>
+          With Chomp magic, this will allow you to see which answers have the
+          most accuracy.
+        </p>
+        <p>Easy enough? Let’s answer your first daily deck!</p>
+        <p>PS. You have a total of 2 minutes to answer each question!</p>
+      </>
+    ),
+  },
+];
