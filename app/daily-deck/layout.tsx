a9f4bfce77a973d84423c1a5928bc43a06efcff1
Bartol Deak <b@bdeak.net>
2024-04-16 10:26:36 +0200

    remove navbar from daily deck page

--- app/daily-deck/layout.tsx
+++ app/daily-deck/layout.tsx
@@ -1,6 +1,4 @@
 import { ReactNode } from "react";
-import AvatarPlaceholder from "../../public/images/avatar_placeholder.png";
-import { Navbar } from "../components/Navbar/Navbar";
 
 type PageLayoutProps = {
   children: ReactNode;
@@ -9,7 +7,6 @@ type PageLayoutProps = {
 export default function Layout({ children }: PageLayoutProps) {
   return (
     <div className="flex flex-col h-full">
-      <Navbar avatarSrc={AvatarPlaceholder.src} avatarLink="" walletLink="" />
       <main className="flex-grow overflow-y-auto mb-2 h-full w-full max-w-lg mx-auto">
         {children}
       </main>
