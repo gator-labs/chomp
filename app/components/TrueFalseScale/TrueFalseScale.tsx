84ca93c2c79ecbb2c895c8dc9c6e158dc63dd415
boranfurkan <furkanboran123@gmail.com>
2024-04-30 23:48:46 +0100

    feat: add new slider into true false component

--- app/components/TrueFalseScale/TrueFalseScale.tsx
+++ app/components/TrueFalseScale/TrueFalseScale.tsx
@@ -1,15 +1,10 @@
-import { useSteppingChange } from "@/app/hooks/useSteppingChange";
-import classNames from "classnames";
-import { useState } from "react";
-import { Avatar } from "../Avatar/Avatar";
-import { ProgressBar } from "../ProgressBar/ProgressBar";
+import PrimarySlider from "../PrimarySlider/PrimarySlider";
 
 type TrueFalseScaleProps = {
-  ratioTrue?: number | null;
-  valueSelected?: number | null;
+  ratioTrue: number;
+  handleRatioChange: (percentage: number) => void;
   avatarSrc?: string;
-  progressBarClassName?: string;
-  handleRatioChange?: (percentage: number) => void;
+  sliderClassName?: string;
   labelTrue?: string;
   labelFalse?: string;
   progressColor?: string;
@@ -18,67 +13,24 @@ type TrueFalseScaleProps = {
 
 export function TrueFalseScale({
   ratioTrue,
-  valueSelected,
-  avatarSrc,
-  progressBarClassName,
   handleRatioChange,
+  avatarSrc,
+  sliderClassName,
   labelTrue = "True",
   labelFalse = "False",
-  progressColor = "#8872A5",
-  bgColor = "#CFC5F7",
+  progressColor,
+  bgColor,
 }: TrueFalseScaleProps) {
-  const avatarLeft = valueSelected
-    ? valueSelected > 90
-      ? "calc(100% - 16px)"
-      : `${valueSelected}%`
-    : undefined;
-  const { handlePercentageChange } = useSteppingChange({
-    percentage: ratioTrue ?? 0,
-    onPercentageChange: handleRatioChange,
-  });
-
-  const [isVisibleBackdrop, setIsVisibleBackdrop] = useState(false);
-
   return (
-    <div className="relative">
-      {!!handleRatioChange && isVisibleBackdrop && (
-        <div className="fixed inset-0 bg-black bg-opacity-80 z-[9999]" />
-      )}
-      {!!handleRatioChange && isVisibleBackdrop && (
-        <div className="absolute px-5 py-4 bg-pink right-0 -top-4 -translate-y-full z-[9999] rounded-xl flex gap-5">
-          <p className="text-[#0d0d0d7d] font-normal">
-            {labelTrue.substring(0, 1)}{" "}
-            <span className="text-[#0D0D0D] font-semibold">{ratioTrue}%</span>
-          </p>
-          <p className="text-[#0d0d0d7d] font-normal">
-            {labelFalse.substring(0, 1)}{" "}
-            <span className="text-[#0D0D0D] font-semibold">
-              {100 - (ratioTrue ?? 0)}%
-            </span>
-          </p>
-        </div>
-      )}
-      <ProgressBar
-        percentage={
-          ratioTrue === undefined || ratioTrue === null ? 100 : ratioTrue
-        }
+    <div className="relative h-max flex flex-col gap-2">
+      <PrimarySlider
+        value={ratioTrue}
+        setValue={handleRatioChange}
         progressColor={progressColor}
-        bgColor={bgColor}
-        className={classNames("h-[21px] z-30", progressBarClassName)}
-        showThumb={!!handleRatioChange}
-        onChange={(percentage) => handlePercentageChange(percentage)}
-        onTouchStart={() => setIsVisibleBackdrop(true)}
-        onTouchEnd={() => setIsVisibleBackdrop(false)}
+        backgroundColor={bgColor}
+        className={sliderClassName}
       />
-      {valueSelected !== undefined && valueSelected !== null && avatarSrc && (
-        <Avatar
-          src={avatarSrc}
-          size="extrasmall"
-          className="absolute top-0.5 z-40"
-          style={{ left: avatarLeft }}
-        />
-      )}
-      <div className="flex justify-between text-white font-sora text-base font-semibold mt-2 z-30 relative">
+      <div className="flex justify-between text-white font-sora text-base font-semibold z-30 relative">
         <span>
           {labelTrue} {ratioTrue ?? "0"}%
         </span>
