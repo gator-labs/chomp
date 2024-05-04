4c5ea1516f4ebcac676195ab58a455d7c793cb42
boranfurkan <furkanboran123@gmail.com>
2024-05-04 03:23:11 +0100

    fix: update true false scale

--- app/components/TrueFalseScale/TrueFalseScale.tsx
+++ app/components/TrueFalseScale/TrueFalseScale.tsx
@@ -1,25 +1,32 @@
+import { Avatar } from "../Avatar/Avatar";
 import PrimarySlider from "../PrimarySlider/PrimarySlider";
 
 type TrueFalseScaleProps = {
   ratioTrue: number;
   handleRatioChange: (percentage: number) => void;
   avatarSrc?: string;
+  valueSelected?: number | null | undefined;
   sliderClassName?: string;
+  trackClassName?: string;
   labelTrue?: string;
   labelFalse?: string;
   progressColor?: string;
   bgColor?: string;
+  hideThumb?: boolean;
 };
 
 export function TrueFalseScale({
   ratioTrue,
   handleRatioChange,
   avatarSrc,
+  valueSelected,
   sliderClassName,
+  trackClassName,
   labelTrue = "True",
   labelFalse = "False",
   progressColor,
   bgColor,
+  hideThumb,
 }: TrueFalseScaleProps) {
   return (
     <div className="relative h-max flex flex-col gap-2">
@@ -29,7 +36,19 @@ export function TrueFalseScale({
         progressColor={progressColor}
         backgroundColor={bgColor}
         className={sliderClassName}
+        trackClassName={trackClassName}
+        hideThumb={hideThumb}
       />
+      {valueSelected !== undefined && valueSelected !== null && avatarSrc && (
+        <Avatar
+          src={avatarSrc}
+          size="extrasmall"
+          className="absolute top-0.5 z-40"
+          style={{
+            left: `calc(${valueSelected}% - 0.5rem)`,
+          }}
+        />
+      )}
       <div className="flex justify-between text-white font-sora text-base font-semibold z-30 relative">
         <span>
           {labelTrue} {ratioTrue ?? "0"}%
