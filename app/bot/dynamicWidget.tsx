17de99abe590da4cf1e426b660084400a049fce8
Marvin Arnold <marvin@geeky.rocks>
2024-05-25 22:17:08 -0500

    feat: dynamic headless login

---
+++ app/bot/dynamicWidget.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+import { DynamicWidget } from "@dynamic-labs/sdk-react-core";
+
+export default function Bot() {
+  return <DynamicWidget />;
+}
--- app/bot/page.tsx
+++ app/bot/page.tsx
@@ -1,7 +1,84 @@
 "use client";
 
-import { DynamicWidget } from "@dynamic-labs/sdk-react-core";
+import {
+  useConnectWithEmailOtp,
+  useDynamicContext,
+  useUserWallets,
+} from "@dynamic-labs/sdk-react-core";
+import { ISolana } from "@dynamic-labs/solana";
+import { Connection } from "@solana/web3.js";
 
-export default function Bot() {
-  return <DynamicWidget />;
-}
+import { FC, FormEventHandler } from "react";
+import { genBonkBurnTx } from "../utils/solana";
+const CONNECTION = new Connection(process.env.NEXT_PUBLIC_RPC_URL!);
+
+const ConnectWithOtpView: FC = () => {
+  const { user } = useDynamicContext();
+  // const { primaryWallet } = useDynamicContext();
+  const userWallets = useUserWallets();
+  console.log(user);
+  console.log("userWallets");
+  console.log(userWallets);
+  const primaryWallet = userWallets.length > 0 ? userWallets[0] : null;
+
+  const { verifyOneTimePassword, connectWithEmail } = useConnectWithEmailOtp();
+
+  const onSubmitEmailHandler: FormEventHandler<HTMLFormElement> = async (
+    event,
+  ) => {
+    event.preventDefault();
+
+    await connectWithEmail(event.currentTarget.email.value);
+  };
+
+  const onSubmitOtpHandler: FormEventHandler<HTMLFormElement> = async (
+    event,
+  ) => {
+    event.preventDefault();
+
+    const otp = event.currentTarget.otp.value;
+
+    await verifyOneTimePassword(otp);
+  };
+
+  const onBurn = async () => {
+    // const onBurn: FormEventHandler<HTMLFormElement> = async () => {
+    console.log("burning");
+    const blockhash = await CONNECTION.getLatestBlockhash();
+    const signer = await primaryWallet!.connector.getSigner<ISolana>();
+    const tx = await genBonkBurnTx(primaryWallet!.address, blockhash.blockhash);
+    const { signature } = await signer.signAndSendTransaction(tx);
+    await CONNECTION.confirmTransaction({
+      blockhash: blockhash.blockhash,
+      lastValidBlockHeight: blockhash.lastValidBlockHeight,
+      signature,
+    });
+  };
+
+  return (
+    <div className="text-black">
+      <form key="sms-form" onSubmit={onSubmitEmailHandler}>
+        <label htmlFor="email">Email:</label>
+        <input type="text" name="email" placeholder="john@example.com" />
+
+        <button type="submit">Submit</button>
+      </form>
+
+      <form key="otp-form" onSubmit={onSubmitOtpHandler}>
+        <input type="text" name="otp" placeholder="OTP" />
+        <button type="submit">Submit</button>
+      </form>
+
+      {!!primaryWallet && <button onClick={onBurn}>Burn Bonk</button>}
+
+      {/* {!!user && (
+        <>
+          <p>Authenticated user:</p>
+          <pre>{JSON.stringify(user, null, 2)}</pre>
+        </>
+      )} */}
+    </div>
+  );
+};
+
+export default ConnectWithOtpView;
