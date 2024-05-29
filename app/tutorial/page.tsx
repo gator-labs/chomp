76897fcaffb53cc6752a3c5ea116e3ca91c593d5
Danijel Radomiljac <danijel.radomiljac@blankhq.co>
2024-05-29 15:21:03 +0200

    build fix

--- app/tutorial/page.tsx
+++ app/tutorial/page.tsx
@@ -7,12 +7,15 @@ import { Navbar } from "../components/Navbar/Navbar";
 import { TabNavigation } from "../components/TabNavigation/TabNavigation";
 
 import ConfettiProvider from "../providers/ConfettiProvider";
+import { getIsUserAdmin } from "../queries/user";
 import TutorialFlowScreens from "../screens/TutorialScreens/TutorialFlowScreens/TutorialFlowScreens";
 
-const TutorialPage = () => {
+const TutorialPage = async () => {
   const currentDate = new Date();
 
   const formattedDate = format(currentDate, "MMMM do EEEE", { locale: enUS });
+
+  const isAdmin = await getIsUserAdmin();
 
   return (
     <ConfettiProvider>
@@ -39,7 +42,7 @@ const TutorialPage = () => {
           <TutorialFlowScreens />
         </main>
 
-        <TabNavigation />
+        <TabNavigation isAdmin={isAdmin} />
       </div>
     </ConfettiProvider>
   );
