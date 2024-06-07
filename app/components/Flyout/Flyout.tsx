2e0324b50bbbbf4af3ca12d152d7385ce9ddb603
Adithya Krishna <aadithya794@gmail.com>
2024-06-08 02:12:11 +0530

    feat: smooth flyout opening
    Signed-off-by: Adithya Krishna <aadithya794@gmail.com>

--- app/components/Flyout/Flyout.tsx
+++ app/components/Flyout/Flyout.tsx
@@ -1,4 +1,4 @@
-import { MouseEventHandler, ReactNode, useRef } from "react";
+import { MouseEventHandler, ReactNode, useEffect, useRef } from "react";
 import { ReactPortal } from "../ReactPortal/ReactPortal";
 
 type FlyoutProps = {
@@ -9,12 +9,35 @@ type FlyoutProps = {
 
 export function Flyout({ isOpen, children, onClose }: FlyoutProps) {
   const ref = useRef<HTMLDivElement>(null);
+  const contentRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    function handleEscapeKey(event: { key: string }) {
+      if (event.key === "Escape") {
+        onClose();
+      }
+    }
+    document.addEventListener("keydown", handleEscapeKey);
+    return () => {
+      document.removeEventListener("keydown", handleEscapeKey);
+    };
+  }, [onClose]);
+
+  useEffect(() => {
+    if (contentRef.current) {
+      if (isOpen) {
+        contentRef.current.classList.add("open");
+      } else {
+        contentRef.current.classList.remove("open");
+      }
+    }
+  }, [isOpen]);
 
   if (!isOpen) {
     return null;
   }
 
-  const handleClickoutside: MouseEventHandler<HTMLDivElement> = (event) => {
+  const handleClickOutside: MouseEventHandler<HTMLDivElement> = (event) => {
     if (event.target === ref.current) {
       onClose();
     }
@@ -24,7 +47,7 @@ export function Flyout({ isOpen, children, onClose }: FlyoutProps) {
     <ReactPortal>
       <div
         ref={ref}
-        onClick={handleClickoutside}
+        onClick={handleClickOutside}
         className="fixed top-0 h-full w-full flex justify-end bg-black bg-opacity-80"
       >
         <div className="bg-search-gray w-[338px] h-full">{children}</div>
