ffa233b5c8a2f857ebb4735d8d2c6093aef6d931
Danijel Radomiljac <danijel.radomiljac@blankhq.co>
2024-06-10 10:04:19 +0200

    fix: genesis reveal

--- app/providers/RevealProvider.tsx
+++ app/providers/RevealProvider.tsx
@@ -166,7 +166,7 @@ export function RevealContextProvider({
   );
 
   const revealButtons = useMemo(() => {
-    if (insufficientFunds) {
+    if (insufficientFunds && !genesisNft) {
       return (
         <>
           <a
@@ -258,7 +258,7 @@ export function RevealContextProvider({
                   : setIsRevealModalOpen(false)
               }
             >
-              {genesisNft && !reveal?.multiple
+              {genesisNft && !reveal?.multiple && !insufficientFunds
                 ? `Reveal for ${numberToCurrencyFormatter.format(reveal?.amount ?? 0)}
               BONK`
                 : "Cancel"}
