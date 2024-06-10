9683a82fbcab56559b1420f1cc2e7536f5a4e990
Marvin Arnold <marvin@geeky.rocks>
2024-06-09 19:32:16 -0500

    wip

--- app/bot/layout.tsx
+++ app/bot/layout.tsx
@@ -1,16 +1,9 @@
 "use client";
 
-import { TurnkeyProvider } from "@turnkey/sdk-react";
-
 export default function BotLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const turnkeyConfig = {
-    apiBaseUrl: process.env.NEXT_PUBLIC_TURNKEY_API_BASE_URL!,
-    defaultOrganizationId: process.env.NEXT_PUBLIC_TURNKEY_ORGANIZATION_ID!,
-    serverSignUrl: process.env.NEXT_PUBLIC_TURNKEY_SIGNER_URL!,
-  };
-  return <TurnkeyProvider config={turnkeyConfig}>{children}</TurnkeyProvider>;
+  return <div>{children}</div>;
 }
