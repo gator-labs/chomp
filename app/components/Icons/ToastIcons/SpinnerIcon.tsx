eaa050a80d4f2c25fc3c60cc0fcd7631b70ec4ef
boranfurkan <furkanboran123@gmail.com>
2024-06-13 02:37:53 +0300

    fix: icon react proporties

--- app/components/Icons/ToastIcons/SpinnerIcon.tsx
+++ app/components/Icons/ToastIcons/SpinnerIcon.tsx
@@ -13,8 +13,8 @@ export function SpinnerIcon({
       stroke="#fff"
     >
       <g fill="none" fillRule="evenodd">
-        <g transform="translate(1 1)" stroke-width="2">
-          <circle stroke-opacity=".2" cx="18" cy="18" r="18" />
+        <g transform="translate(1 1)" strokeWidth="2">
+          <circle strokeOpacity=".2" cx="18" cy="18" r="18" />
           <path d="M36 18c0-9.94-8.06-18-18-18">
             <animateTransform
               attributeName="transform"
