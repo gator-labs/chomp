7855de8f0aaa8968046bb499a01d031ba481584a
boranfurkan <furkanboran123@gmail.com>
2024-06-22 20:58:00 +0300

    feat: complete pause screen

--- app/screens/DailyDeckScreen/DailyDeckScreen.tsx
+++ app/screens/DailyDeckScreen/DailyDeckScreen.tsx
@@ -4,7 +4,6 @@ import { DailyDeckTitle } from "@/app/components/DailyDeckTitle/DailyDeckTitle";
 import { Deck, Question } from "@/app/components/Deck/Deck";
 import Disabled from "@/app/components/Disabled/Disabled";
 import { Navbar, NavbarProps } from "@/app/components/Navbar/Navbar";
-import { NoQuestionsCard } from "@/app/components/NoQuestionsCard/NoQuestionsCard";
 import { TabNavigation } from "@/app/components/TabNavigation/TabNavigation";
 import { useState } from "react";
 
@@ -26,47 +25,38 @@ const DailyDeckScreen = ({
   const [hasReachedEnd, setHasReachedEnd] = useState(!questions?.length);
 
   return (
-    <>
-      <div className="flex flex-col h-full">
-        <main className="flex-grow overflow-y-auto mb-2 h-full w-full max-w-lg mx-auto">
-          <div className="flex flex-col h-full px-4">
-            <Disabled
-              disabled={!hasReachedEnd}
-              toastMessage="Please complete this Daily Deck first ✨"
-            >
-              <Navbar {...navBarData} />
-            </Disabled>
+    <div className="flex flex-col h-full">
+      <main className="flex-grow overflow-y-auto mb-2 h-full w-full max-w-lg mx-auto">
+        <div className="flex flex-col h-full px-4">
+          <Disabled
+            disabled={!hasReachedEnd}
+            toastMessage="Please complete this Daily Deck first ✨"
+          >
+            <Navbar {...navBarData} />
+          </Disabled>
 
-            <div className="py-3">
-              <DailyDeckTitle date={date ?? new Date()} />
-            </div>
-            <div className="flex-1">
-              {!!questions?.length ? (
-                <Deck
-                  deckVariant="daily-deck"
-                  questions={questions}
-                  deckId={id!}
-                  browseHomeUrl="/application"
-                  setHasReachedEnd={setHasReachedEnd}
-                />
-              ) : (
-                <NoQuestionsCard
-                  browseHomeUrl="/application"
-                  variant="daily-deck"
-                />
-              )}
-            </div>
+          <div className="py-3">
+            <DailyDeckTitle date={date ?? new Date()} />
           </div>
-        </main>
-        <Disabled
-          className="after:opacity-90 after:bg-[#1B1B1B]"
-          disabled={!hasReachedEnd}
-          toastMessage="Please complete this Daily Deck first ✨"
-        >
-          <TabNavigation isAdmin={isAdmin} />
-        </Disabled>
-      </div>
-    </>
+          <div className="flex-1">
+            <Deck
+              deckVariant="daily-deck"
+              questions={questions || []}
+              deckId={id!}
+              browseHomeUrl="/application"
+              setHasReachedEnd={setHasReachedEnd}
+            />
+          </div>
+        </div>
+      </main>
+      <Disabled
+        className="after:opacity-90 after:bg-[#1B1B1B]"
+        disabled={!hasReachedEnd}
+        toastMessage="Please complete this Daily Deck first ✨"
+      >
+        <TabNavigation isAdmin={isAdmin} />
+      </Disabled>
+    </div>
   );
 };
 
