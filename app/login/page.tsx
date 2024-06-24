c40b94e07464be076ea98314de785bcf182d1b68
Danijel Radomiljac <danijel.radomiljac@blankhq.co>
2024-06-24 20:35:35 +0200

    remove revalidate

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,8 +2,6 @@ import { getJwtPayload } from "../actions/jwt";
 import { getDailyDeck } from "../queries/deck";
 import LoginScreen from "../screens/LoginScreens/LoginScreen";
 
-export const revalidate = 0;
-
 export default async function Page() {
   const dailyDeck = await getDailyDeck();
   const payload = await getJwtPayload();
