5b55ee377c08e03966e7621e1d142c72b91823cb
Yash Garg <yash431garg@gmail.com>
2024-07-02 15:30:41 +0530

    feat: added error handeling in tiplink connection and copy address button

--- app/bot/tiplinkConnect.tsx
+++ app/bot/tiplinkConnect.tsx
@@ -2,15 +2,22 @@ import { GoogleViaTipLinkWalletName } from '@tiplink/wallet-adapter'
 import { useWallet } from '@solana/wallet-adapter-react'
 import { CONNECTION, genBonkBurnTx, getBonkBalance } from '../utils/solana';
 
-import { useToast } from "../providers/ToastProvider";
 import { useState } from 'react';
+import { CopyIcon } from '@dynamic-labs/sdk-react-core';
+import { Button } from '../components/Button/Button';
+
+import { copyTextToClipboard } from "../utils/clipboard";
+import { useToast } from "../providers/ToastProvider";
+import { formatAddress } from '../utils/wallet';
+
 
 
 function TiplinkConnect() {
 
-    const { select, connect, publicKey, sendTransaction } = useWallet();
+    const { select, connect, publicKey, sendTransaction, wallet } = useWallet();
 
-    const { errorToast } = useToast();
+    const { successToast, errorToast } = useToast();
+    const address = wallet?.adapter?._wallet?.publicKeyString;
 
     const [burned, setBurned] = useState(false);
 
@@ -56,15 +63,26 @@ function TiplinkConnect() {
         }
         catch (error) {
             console.log(error)
+            errorToast("Error in Bunrn transaction");
         }
     };
+
+    const handleCopyToClipboard = async () => {
+        await copyTextToClipboard(address);
+        successToast(
+            "Copied to clipboard",
+            `Copied ${formatAddress(address)} to clipboard`,
+        );
+    };
+
+
     return (
         <div>
             {burned ?
-                <p className="text-2xl text-[#A3A3EC]">
+                <p className="text-2xl text-[#A3A3EC] text-center">
                     BONK burn and reveal successful! Return to Telegram.
                 </p> :
-                <p className='pb-2'>
+                <p className='pb-2 text-center'>
                     You have 8 questions to reveal. You must reveal in order to see the
                     answer and find out if you won anything.
                 </p>
@@ -79,12 +97,32 @@ function TiplinkConnect() {
                     </button>
                     :
                     !burned ?
-                        <button
-                            className="w-full rounded-sm bg-[#A3A3EC] text-xl p-2"
-                            onClick={onBurn}
-                        >
-                            Burn
-                        </button> : ''}
+                        <>
+
+                            {address !== '' &&
+                                <div className="text-center flex flex-col ml">
+                                    Your address:
+                                    <p className="whitespace-nowrap overflow-hidden text-ellipsis text-center ">
+                                        {address}
+                                    </p>
+
+                                    <span>
+                                        <Button
+                                            isPill
+                                            className="!p-0 !w-[28px] !h-[28px] bg-[#A3A3EC] border-none ml-2"
+                                            onClick={handleCopyToClipboard}
+                                        >
+                                            <CopyIcon />
+                                        </Button>
+                                    </span>
+                                </div>}
+
+                            <button
+                                className="w-full rounded-sm bg-[#A3A3EC] text-xl p-2 mt-2"
+                                onClick={onBurn}
+                            >
+                                Burn
+                            </button> </> : ''}
         </div>
     )
 
