ffc8fcbdec49d96ba0bcef903bc8e10ee707f501
Neel <neelp0980@gmail.com>
2024-07-09 22:02:06 +0530

    chore: remove schema changes

--- prisma/migrations/20240527023553_add_dynamic_id/migration.sql
+++
@@ -1,6 +0,0 @@
--- AlterTable
-ALTER TABLE "User" ADD COLUMN     "dynamicUserId" TEXT,
-ALTER COLUMN "id" SET DEFAULT gen_random_uuid();
-
--- Copy existing id values to dynamicUserId
-UPDATE "User" SET "dynamicUserId" = id;
