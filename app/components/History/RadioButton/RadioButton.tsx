71d6a2b872124ec7c920d935621c121d28eab9ab
Danijel Radomiljac <danijel.radomiljac@blankhq.co>
2024-07-11 12:39:44 +0200

    add attributes

--- app/components/History/RadioButton/RadioButton.tsx
+++ app/components/History/RadioButton/RadioButton.tsx
@@ -7,16 +7,14 @@ interface RadioButtonProps extends InputHTMLAttributes<HTMLInputElement> {
 
 export default function RadioButton({
   text,
-  value,
   checked,
-  onChange,
+  ...attributes
 }: RadioButtonProps) {
   return (
     <div className="flex items-center">
       <input
         id="default-radio-1"
         type="radio"
-        value={value}
         name="default-radio"
         className={cn(
           "appearance-none w-6 h-6 border-2 rounded-[40px] border-[#999999] bg-[#FFFFFF]",
@@ -24,8 +22,8 @@ export default function RadioButton({
             "border-[#CFC5F7]": checked,
           },
         )}
-        onChange={onChange}
         checked={checked}
+        {...attributes}
       />
       {checked && (
         <div className="absolute w-4 h-4 rounded-full bg-[#A3A3EC] ml-1" />
