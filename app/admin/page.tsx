c1fa178d19b0e04b275f462724438c790a308b70
Danijel Radomiljac <danijel.radomiljac@blankhq.co>
2024-07-16 11:45:11 +0200

    add cl

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,3 +1,7 @@
 export default async function Page() {
+  console.log({
+    url: process.env.KV_REST_API_URL,
+    token: process.env.KV_REST_API_TOKEN,
+  });
   return <div>some app stats</div>;
 }
