581b89f85316e575aacdadf0d0ce5642bbb37063
Danijel Radomiljac <danijel.radomiljac@blankhq.co>
2024-07-16 11:51:11 +0200

    rm log

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,7 +1,3 @@
 export default async function Page() {
-  console.log({
-    url: process.env.KV_REST_API_URL,
-    token: process.env.KV_REST_API_TOKEN,
-  });
   return <div>some app stats</div>;
 }
