74f832257c898f9093e8e65c2d7597cfdd3b996d
Danijel Radomiljac <danijel.radomiljac@blankhq.co>
2024-07-22 16:46:52 +0200

    application error fix

--- app/components/Deck/Deck.tsx
+++ app/components/Deck/Deck.tsx
@@ -66,7 +66,7 @@ export function Deck({
   const [optionPercentage, setOptionPercentage] = useState(50);
   const min = 0;
   const max =
-    questions[currentQuestionIndex] &&
+    !!questions[currentQuestionIndex] &&
     questions[currentQuestionIndex].questionOptions.length > 0
       ? questions[currentQuestionIndex].questionOptions.length - 1
       : 0;
@@ -92,7 +92,13 @@ export function Deck({
     setRerenderAction(false);
     setOptionPercentage(50);
     setCurrentOptionSelected(undefined);
-    generateRandom();
+    const min = 0;
+    const max =
+      !!questions[currentQuestionIndex + 1] &&
+      questions[currentQuestionIndex + 1].questionOptions.length > 0
+        ? questions[currentQuestionIndex + 1].questionOptions.length - 1
+        : 0;
+    generateRandom({ min, max });
     reset();
     setTimeout(() => {
       setRerenderAction(true);
@@ -110,7 +116,7 @@ export function Deck({
     generateRandom,
     setCurrentOptionSelected,
     reset,
-    questionsRef.current,
+    questions,
   ]);
 
   const question = useMemo(
