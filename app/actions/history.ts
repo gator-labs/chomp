8a0b613abb5e58c49a6ea5abf5d9cdd09b13b2ab
Danijel Radomiljac <danijel.radomiljac@blankhq.co>
2024-07-26 15:30:26 +0200

    query optimization

--- app/actions/history.ts
+++ app/actions/history.ts
@@ -1,6 +1,11 @@
 "use server";
 
-import { getDecksHistory } from "../queries/history";
+import {
+  getDecksHistory,
+  getQuestionsHistoryQuery,
+  QuestionHistory,
+} from "../queries/history";
+import prisma from "../services/prisma";
 import { getJwtPayload } from "./jwt";
 
 export const getHistoryDecks = async () => {
@@ -12,3 +17,37 @@ export const getHistoryDecks = async () => {
 
   return getDecksHistory(payload.sub);
 };
+
+export const getQuestionsHistory = async (): Promise<QuestionHistory[]> => {
+  const payload = await getJwtPayload();
+
+  if (!payload?.sub) {
+    return [];
+  }
+
+  return getQuestionsHistoryQuery(payload.sub);
+};
+
+export async function getTotalClaimableRewards(): Promise<number> {
+  const payload = await getJwtPayload();
+
+  if (!payload) {
+    return 0;
+  }
+
+  const res = await prisma.chompResult.aggregate({
+    where: {
+      userId: payload.sub,
+      result: "Revealed",
+      questionId: { not: null },
+      rewardTokenAmount: {
+        gt: 0,
+      },
+    },
+    _sum: {
+      rewardTokenAmount: true,
+    },
+  });
+
+  return Math.trunc(Number(res._sum.rewardTokenAmount));
+}
--- app/queries/history.ts
+++ app/queries/history.ts
@@ -1,217 +1,109 @@
-import { redirect } from "next/navigation";
-import { getAllRevealableQuestions } from "../actions/claim";
-import { getJwtPayload } from "../actions/jwt";
 import prisma from "../services/prisma";
-import { onlyUniqueBy } from "../utils/array";
 
 export type HistoryResult = {
   id: number;
-  question: string;
-  revealAtDate: Date;
-  revealAtAnswerCount: number;
-  revealTokenAmount: number;
-  isRevealable: boolean;
-  isRevealed: boolean;
-  isClaimed: boolean;
-  isChomped: boolean;
-  type: "Question" | "Deck";
+  image: string | null;
+  revealAtDate: Date | null;
+  deck: string;
+  numberOfQuestionsInDeck: number;
+  answeredQuestions: number;
+  claimedQuestions: number;
+  revealedQuestions: number;
+  claimableQuestions: number;
+  claimedAmount: number | null;
 };
 
-export enum HistorySortOptions {
-  Date = "Date",
-  Revealed = "Revealed",
-  Claimable = "Claimable",
-}
-
-export enum HistoryTypeOptions {
-  Deck = "Deck",
-  Question = "Question",
-}
-
-export async function getTotalClaimableRewards(): Promise<number> {
-  const payload = await getJwtPayload();
-
-  if (!payload) {
-    return redirect("/login");
-  }
-
-  const res = await prisma.chompResult.aggregate({
-    where: {
-      userId: payload.sub,
-      result: "Revealed",
-      questionId: { not: null },
-      rewardTokenAmount: {
-        gt: 0,
-      },
-    },
-    _sum: {
-      rewardTokenAmount: true,
-    },
-  });
-
-  return Math.trunc(Number(res._sum.rewardTokenAmount));
-}
-
-export async function getTotalRevealedRewards(): Promise<number> {
-  const payload = await getJwtPayload();
-
-  if (!payload) {
-    return redirect("/login");
-  }
-
-  const revealableQuestions = await getAllRevealableQuestions();
-  const totalRevealedRewards = revealableQuestions!
-    .filter(onlyUniqueBy((x) => x.burnTransactionSignature))
-    .reduce((acc, curr) => acc + (curr.rewardTokenAmount?.toNumber() ?? 0), 0);
-
-  return totalRevealedRewards;
-}
-
-export function getSortClause(sort: string): string {
-  switch (sort) {
-    case HistorySortOptions.Claimable:
-      return '"isRevealed" desc, "revealAtDate"';
-    case HistorySortOptions.Date:
-      return '"revealAtDate" DESC';
-    case HistorySortOptions.Revealed:
-      return '"isRevealable" desc, "revealAtDate"';
-    default:
-      return '"revealAtDate"';
-  }
-}
+export type QuestionHistory = {
+  id: number;
+  question: string;
+  revealAtDate: Date;
+  isAnswered: boolean;
+  isClaimed: boolean;
+  isRevealed: boolean;
+  isClaimable: boolean;
+  isRevealable: boolean;
+  claimedAmount?: number;
+};
 
 export async function getDecksHistory(
   userId: string,
 ): Promise<HistoryResult[]> {
   const decksHistory: HistoryResult[] = await prisma.$queryRawUnsafe(
-    `select distinct
-    d."id",
-    d."deck" AS "question",
-		'Deck' AS "type",
-    d."revealAtDate",
-				(
-					(
-						d."revealAtDate" IS NOT NULL
-							AND
-						d."revealAtDate" < NOW()
-						)
-						OR
-					(
-						d."revealAtAnswerCount" IS NOT NULL
-							AND
-						d."revealAtAnswerCount" >=
-						(SELECT COUNT(DISTINCT CONCAT(qa."userId", dq."deckId"))
-							FROM public."QuestionOption" qo
-									JOIN public."QuestionAnswer" qa ON qa."questionOptionId" = qo."id"
-									JOIN public."DeckQuestion" dq ON dq."questionId" = qo."questionId"
-							WHERE dq."deckId" = d."id")
-						)
-					) AS "isRevealable",
-				(
-					d."id" IN
-					(SELECT cr."deckId"
-						FROM public."ChompResult" cr
-						WHERE cr."deckId" = d."id"
-						AND cr."userId" = '${userId}'
-						AND cr."result" = 'Revealed')
-					)                        AS "isRevealed",
-				(
-					d."id" IN
-					(SELECT cr."deckId"
-						FROM public."ChompResult" cr
-						WHERE cr."deckId" = d."id"
-						AND cr."userId" = '${userId}'
-						AND cr."result" = 'Claimed')
-					)                        AS "isClaimed",
-				(
-					(SELECT COUNT(DISTINCT dq."deckId" + dq."questionId")
-						FROM public."QuestionOption" qo
-								JOIN public."QuestionAnswer" qa ON qa."questionOptionId" = qo."id"
-								JOIN public."DeckQuestion" dq ON dq."questionId" = qo."questionId"
-						WHERE qa."userId" = '${userId}'
-						AND dq."deckId" = d."id"
-						LIMIT 1)
-						=
-					(SELECT COUNT(DISTINCT dq."deckId" + dq."questionId")
-						FROM public."DeckQuestion" dq
-						WHERE dq."deckId" = d."id"
-						LIMIT 1)
-					)                        AS "isChomped",
-    d."revealAtAnswerCount"
-		FROM public."Deck" d
-		JOIN public."DeckQuestion" dq ON dq."deckId" = d."id"
-		JOIN public."QuestionOption" qo ON qo."questionId" = dq."questionId"
-		JOIN public."QuestionAnswer" qa ON qa."questionOptionId" = qo."id"
-		WHERE qa."userId" = '${userId}';`,
+    `
+		SELECT 
+			d.id,
+			c.image,
+			d."revealAtDate",
+			d.deck, 
+			COUNT(DISTINCT dq."questionId") AS "numberOfQuestionsInDeck", 
+			COUNT(DISTINCT CASE WHEN qa.selected = true THEN qa."questionOptionId" END) AS "answeredQuestions",
+			COUNT(DISTINCT CASE WHEN cr."result" = 'Claimed'::public."ResultType" AND cr."rewardTokenAmount" > 0 THEN cr."questionId" END) AS "claimedQuestions",
+			COUNT(DISTINCT CASE WHEN cr."result" = 'Revealed'::public."ResultType" OR cr."result" = 'Claimed'::public."ResultType" THEN cr."questionId" END) AS "revealedQuestions",
+			COUNT(DISTINCT CASE WHEN cr."result" = 'Revealed'::public."ResultType" AND cr."rewardTokenAmount" > 0 THEN cr."questionId" END) AS "claimableQuestions",
+			SUM(CASE WHEN cr."result" = 'Claimed'::public."ResultType" THEN cr."rewardTokenAmount" END) AS "claimedAmount"
+		FROM "DeckQuestion" dq 
+		JOIN "Deck" d ON d.id = dq."deckId" 
+		JOIN "Question" q ON q.id = dq."questionId"
+		JOIN "QuestionOption" qo ON qo."questionId" = q.id
+		LEFT JOIN "QuestionAnswer" qa ON qa."questionOptionId" = qo.id AND qa."userId" = '${userId}'
+		LEFT JOIN "ChompResult" cr ON cr."questionId" = q.id AND cr."userId" = '${userId}' AND cr."questionId" IS NOT NULL
+		LEFT JOIN "Campaign" c ON c.id = d."campaignId"
+		WHERE d."revealAtDate" IS NOT NULL 
+		GROUP BY d.deck, d.id, c.image, d."revealAtDate"
+		ORDER BY d."revealAtDate" DESC
+		`,
   );
+
+  console.log(decksHistory);
 
   return decksHistory;
 }
 
-export async function getQuestionsHistory(
+export async function getQuestionsHistoryQuery(
   userId: string,
-  sortClause: string,
-): Promise<HistoryResult[]> {
-  const historyResult: HistoryResult[] = await prisma.$queryRawUnsafe(
+): Promise<QuestionHistory[]> {
+  const historyResult: QuestionHistory[] = await prisma.$queryRawUnsafe(
     `
-			SELECT  * FROM (
-				SELECT  q."id",
-						q."question",
-						q."revealAtDate",
-						(SELECT count(distinct concat(qa."userId", qo."questionId"))
-						FROM public."QuestionOption" qo
-								join public."QuestionAnswer" qa ON qa."questionOptionId" = qo."id"
-						where qo."questionId" = q."id") AS "answerCount",
-						q."revealAtAnswerCount",
-						q."revealTokenAmount",
-						(
-							(
-								q."revealAtDate" is not null
-									and
-								q."revealAtDate" < now()
-								)
-								or
-							(
-								q."revealAtAnswerCount" is not null
-									and
-								q."revealAtAnswerCount" >=
-								(SELECT count(distinct concat(qa."userId", qo."questionId"))
-								FROM public."QuestionOption" qo
-										join public."QuestionAnswer" qa ON qa."questionOptionId" = qo."id"
-								where qo."questionId" = q."id")
-								)
-							)                            AS "isRevealable",
-						(
-							q."id" IN
-							(SELECT cr."questionId"
-							FROM public."ChompResult" cr
-							where cr."questionId" = q."id"
-							and cr."userId" = '${userId}'
-							and cr."result" = 'Revealed')
-							)                            AS "isRevealed",
-						(
-							q."id" IN
-							(SELECT cr."questionId"
-							FROM public."ChompResult" cr
-							where cr."questionId" = q."id"
-							and cr."userId" = '${userId}'
-							and cr."result" = 'Claimed')
-							)                            AS "isClaimed",
-						true                             AS "isChomped",
-						'Question'                       AS "type"
-				FROM public."Question" q
-				where q."id" IN
-					(SELECT qo."questionId"
-						FROM public."QuestionOption" qo
-								join public."QuestionAnswer" qa ON qa."questionOptionId" = qo."id"
-						where qa."userId" = '${userId}'
-						and qo."questionId" = q."id")
-				and q."id" not IN
-					(SELECT dq."questionId"
-						FROM public."DeckQuestion" dq
-						where dq."questionId" = q."id")) AS QuestionHistory
-				ORDER BY ${sortClause};
-		`,
+		SELECT 
+				q.id, 
+				q.question,
+				q."revealAtDate",
+				cr."rewardTokenAmount" as "claimedAmount",
+				CASE 
+						WHEN COUNT(CASE WHEN qa.selected = true THEN 1 ELSE NULL END) > 0 THEN true
+						ELSE false 
+				END AS "isAnswered",
+				CASE 
+						WHEN COUNT(CASE WHEN cr.result = 'Claimed' AND cr."rewardTokenAmount" > 0 THEN 1 ELSE NULL END) > 0 THEN true
+						ELSE false 
+				END AS "isClaimed",
+				CASE 
+						WHEN COUNT(CASE WHEN (cr.result = 'Claimed' AND cr."rewardTokenAmount" > 0) OR cr.result = 'Revealed' THEN 1 ELSE NULL END) > 0 THEN true
+						ELSE false 
+				END AS "isRevealed",
+				CASE 
+						WHEN COUNT(CASE WHEN cr.result = 'Revealed' AND cr."rewardTokenAmount" > 0 THEN 1 ELSE NULL END) > 0
+								AND COUNT(CASE WHEN cr.result = 'Claimed' AND cr."rewardTokenAmount" > 0 THEN 1 ELSE NULL END) = 0 THEN true
+						ELSE false 
+				END AS "isClaimable",
+				CASE 
+        		WHEN COUNT(CASE WHEN cr.result = 'Claimed' OR cr.result = 'Revealed' THEN 1 ELSE NULL END) = 0
+        			AND q."revealAtDate" < NOW() THEN true
+        		ELSE false 
+    		END AS "isRevealable"
+		FROM 
+				"Question" q 
+		JOIN 
+				"QuestionOption" qo ON qo."questionId" = q.id 
+		LEFT JOIN 
+				"QuestionAnswer" qa ON qa."questionOptionId" = qo.id AND qa."userId" = '${userId}'
+		LEFT JOIN 
+				"ChompResult" cr ON cr."questionId" = q.id AND cr."userId" = '${userId}' AND cr."questionId" IS NOT NULL
+		WHERE q."revealAtDate" IS NOT NULL
+		GROUP BY 
+				q.id, cr."rewardTokenAmount"
+		ORDER BY 
+				q."createdAt" DESC		`,
   );
 
   return historyResult;
