15ed70c0e66e1a74667ef8337cb4d7e60196c10c
Danijel Radomiljac <danijel.radomiljac@blankhq.co>
2024-07-26 15:31:00 +0200

    add skeleton loader

--- app/components/HistoryListSkeleton/HistoryListSkeleton.tsx
+++ app/components/HistoryListSkeleton/HistoryListSkeleton.tsx
@@ -4,14 +4,11 @@ const HistoryListSkeleton = () => {
   const skeletons = new Array(8).fill(null);
 
   return (
-    <div className="flex flex-col gap-2">
-      <Skeleton className="h-[19px] w-[81px] rounded-[56px]" />
-      <ul className="flex flex-col gap-2">
-        {skeletons.map((_, i) => (
-          <Skeleton key={i} />
-        ))}
-      </ul>
-    </div>
+    <ul className="flex flex-col gap-2">
+      {skeletons.map((_, i) => (
+        <Skeleton key={i} />
+      ))}
+    </ul>
   );
 };
 
