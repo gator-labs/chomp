38d49e3fbcbc16cc1e0b59edc1005c188c3089d4
Danijel Radomiljac <danijel.radomiljac@blankhq.co>
2024-08-14 11:41:31 +0200

    scroll leaderboard name fix

--- app/components/LeaderboardCard/LeaderboardCard.tsx
+++ app/components/LeaderboardCard/LeaderboardCard.tsx
@@ -1,9 +1,13 @@
+"use client";
+
 /* eslint-disable @next/next/no-img-element */
+import useIsOverflowing from "@/app/hooks/useIsOverflowing";
 import Image from "next/image";
 import Link from "next/link";
-import { ReactNode } from "react";
+import { ReactNode, useRef } from "react";
 import ActiveIndicator from "../ActiveIndicator/ActiveIndicator";
 import { HalfArrowRightIcon } from "../Icons/HalfArrowRightIcon";
+import Marquee from "../Marquee/Marquee";
 
 interface Props {
   href: string;
@@ -22,11 +26,14 @@ const LeaderboardCard = ({
   isActive = false,
   showActiveIndicator,
 }: Props) => {
+  const leaderboardNameRef = useRef(null);
+  const isOverflowing = useIsOverflowing(leaderboardNameRef);
+
   return (
     <Link href={href}>
       <li className="px-4 py-[15px] border-[0.5px] border-[#666666] rounded-lg bg-[#333333] flex items-center justify-between h-[70px]">
-        <div className="flex items-center gap-4">
-          <div className="relative w-[38px] h-[38px]">
+        <div className="flex items-center gap-4 overflow-hidden">
+          <div className="relative w-[38px] h-[38px] flex-shrink-0">
             {showActiveIndicator && <ActiveIndicator isActive={isActive} />}
             {imageSrc && (
               <Image
@@ -38,9 +45,21 @@ const LeaderboardCard = ({
             )}
             {icon}
           </div>
-          <p className="text-base">{name}</p>
+
+          {isOverflowing ? (
+            <Marquee text={name} />
+          ) : (
+            <p
+              ref={leaderboardNameRef}
+              className="text-base text-nowrap overflow-hidden flex-1"
+            >
+              {name}
+            </p>
+          )}
         </div>
-        <HalfArrowRightIcon />
+        <div className="flex-shrink-0">
+          <HalfArrowRightIcon />
+        </div>
       </li>
     </Link>
   );
