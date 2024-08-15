923b90dfbde81c8defd361dfcd58e3b96a0164ea
AndrejRadomiljac <andrej.web3@gmail.com>
2024-08-15 12:45:25 +0200

    PROD-130 moved script logic to migration

--- prisma/migrations/20240815095358_remove_duplicated_question_answer_records_and_add_unique_constraint/migration.sql
+++ prisma/migrations/20240815095358_remove_duplicated_question_answer_records_and_add_unique_constraint/migration.sql
@@ -1,8 +1,26 @@
+DELETE FROM public."QuestionAnswer"
+WHERE "hasViewedButNotSubmitted" = true;
+WITH DuplicateRecords AS (
+  SELECT id,
+    ROW_NUMBER() OVER (
+      PARTITION BY "userId",
+      "questionOptionId"
+      ORDER BY "createdAt" DESC,
+        id DESC
+    ) AS row_num
+  FROM public."QuestionAnswer"
+)
+DELETE FROM public."QuestionAnswer"
+WHERE id IN (
+    SELECT id
+    FROM DuplicateRecords
+    WHERE row_num > 1
+  );
 /*
-  Warnings:
-
-  - A unique constraint covering the columns `[questionOptionId,userId]` on the table `QuestionAnswer` will be added. If there are existing duplicate values, this will fail.
-
-*/
+ Warnings:
+ 
+ - A unique constraint covering the columns `[questionOptionId,userId]` on the table `QuestionAnswer` will be added. If there are existing duplicate values, this will fail.
+ 
+ */
 -- CreateIndex
 CREATE UNIQUE INDEX "QuestionAnswer_questionOptionId_userId_key" ON "QuestionAnswer"("questionOptionId", "userId");
