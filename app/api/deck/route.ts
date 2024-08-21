31f43634ab395272175d9739c737493843415a9b
Danijel Radomiljac <danijel.radomiljac@blankhq.co>
2024-08-21 23:37:30 +0200

    add auth guard

--- app/api/deck/route.ts
+++ app/api/deck/route.ts
@@ -1,6 +1,18 @@
 import { getTotalNumberOfAnswersInDeck } from "@/app/actions/deck/deck";
+import { getIsUserAdmin } from "@/app/queries/user";
 
 export async function GET(request: Request) {
+  const isAdmin = await getIsUserAdmin();
+
+  if (!isAdmin) {
+    return new Response(
+      JSON.stringify({
+        error: "You are not authorized to perform this action.",
+      }),
+      { status: 403 },
+    );
+  }
+
   const url = new URL(request.url);
   const deckId = Number(url.searchParams.get("deckId"));
 
