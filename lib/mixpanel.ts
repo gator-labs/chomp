70f1c516b61a8ba26e39f4abe9e83a6f5a9d815a
Danijel Radomiljac <danijel.radomiljac@blankhq.co>
2024-08-30 14:15:03 +0200

    add ipAddress

--- lib/mixpanel.ts
+++ lib/mixpanel.ts
@@ -1,14 +1,26 @@
 import { getJwtPayload } from "@/app/actions/jwt";
 
+const getIpAddress = async () => {
+  try {
+    const response = await fetch("https://api.ipify.org?format=json");
+    const data = await response.json();
+    return data.ip;
+  } catch (error) {
+    console.error("Failed to fetch IP address:", error);
+    return undefined;
+  }
+};
+
 const sendToMixpanel = async (
   eventName: string,
   eventProperties?: Record<string, any>,
 ) => {
   const payload = await getJwtPayload();
-
   if (!payload) return;
 
   const userUUID = payload.sub;
+
+  const ipAddress = await getIpAddress();
 
   const additionalProperties = {
     distinct_id: userUUID,
@@ -22,6 +34,7 @@ const sendToMixpanel = async (
       : undefined,
     $screen_height: window.screen.height,
     $screen_width: window.screen.width,
+    ip: ipAddress,
   };
   const properties = {
     ...eventProperties,
