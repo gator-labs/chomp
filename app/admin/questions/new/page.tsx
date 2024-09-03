a3b877f6ce31c46f39a08c57cb62db7d4592ed5d
Danijel Radomiljac <danijel.radomiljac@blankhq.co>
2024-09-03 10:03:45 +0200

    remove question page

--- app/admin/questions/new/page.tsx
+++
@@ -1,9 +0,0 @@
-import { createQuestion } from "@/app/actions/question/question";
-import QuestionForm from "@/app/components/QuestionForm/QuestionForm";
-import { getTags } from "@/app/queries/tag";
-
-export default async function Page() {
-  const tags = await getTags();
-
-  return <QuestionForm action={createQuestion} tags={tags} />;
-}
