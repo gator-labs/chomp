7ad2cb89f5a837a1caeae7ff887a4383f6e06af1
Danijel Radomiljac <danijel.radomiljac@blankhq.co>
2024-09-04 11:37:31 +0200

    remove dollar sign

--- app/components/NoQuestionsCard/constants.tsx
+++ app/components/NoQuestionsCard/constants.tsx
@@ -22,11 +22,15 @@ export const QUESTION_CARD_CONTENT = {
     body: (date?: Date | null) => (
       <>
         You just chomped through that deck!{" "}
-        {!!date &&
-          `The deck will be revealed in
-        <span className="text-purple-500">
-          ${formatDistanceToNowStrict(date)}
-        </span>.`}
+        {!!date && (
+          <>
+            The deck will be revealed in
+            <span className="text-purple-500">
+              {formatDistanceToNowStrict(date)}
+            </span>
+            .
+          </>
+        )}
         <br />
         <br /> Meanwhile, go check out some more chomps in{" "}
         <b>
