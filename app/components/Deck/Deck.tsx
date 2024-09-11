72811800be1630afd02c000fc1ccefb10f8ae9c6
Yash Garg <yash431garg@gmail.com>
2024-09-11 21:35:16 +0530

    fix: added skip button again.

--- app/components/Deck/Deck.tsx
+++ app/components/Deck/Deck.tsx
@@ -306,7 +306,13 @@ export function Deck({
         setPercentage={setOptionPercentage}
         disabled={isSubmitting}
       />
-
+      <div
+        className="text-sm text-center mt-5 text-gray-400 underline cursor-pointer"
+        onClick={() => handleSkipQuestion()}
+      >
+        Skip question
+      </div>
+      
       <AlertDialog open={isTimeOutPopUpVisible}>
         <AlertDialogContent onEscapeKeyDown={(e) => e.preventDefault()}>
           <AlertDialogHeader>
