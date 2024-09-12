9f20d0bd57568bed0eddaf626f00591f9d791799
Yash Garg <yash431garg@gmail.com>
2024-09-12 14:35:20 +0530

    wip: test daily deck redirect

--- app/components/DailyDeckRedirect/DailyDeckRedirect.tsx
+++ app/components/DailyDeckRedirect/DailyDeckRedirect.tsx
@@ -4,7 +4,7 @@ import { redirect } from "next/navigation";
 export const DailyDeckRedirect = async () => {
   const dailyDeck = await getDailyDeck();
   
-  if (dailyDeck && dailyDeck.questions.length > 0 && dailyDeck?.questions.filter((q) => q.status === undefined).length === dailyDeck?.questions.length) {
+  if (dailyDeck && dailyDeck.questions.length > 0 && dailyDeck?.questions.some((q) => q.status !== undefined)) {
     redirect("/daily-deck");
   }
 
