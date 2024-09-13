1ae95b12a83fd0ff66a0bd234fd89328fe930c6e
Neel <neelp0980@gmail.com>
2024-09-13 10:36:31 +0800

    chore: filter sentry error

--- app/utils/sentry.ts
+++ app/utils/sentry.ts
@@ -3,4 +3,5 @@ export const ignoreErrors = [
   "Object captured as promise rejection with keys: code, data, message",
   "[PhantomRedirect] getSession called, but required input 'encryptionSecretKey' not found in local storage",
   "User rejected the request.",
+  "Talisman extension has not been configured yet. Please continue with onboarding.",
 ];
