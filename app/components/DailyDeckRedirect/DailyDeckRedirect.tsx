55eb869d06338ed708a3bcfe72f5edb762d73128
Andrej Radomiljac <andrej.web3@gmail.com>
2024-09-15 10:54:34 +0200

    PROD-292 added redirect check for direct deck link (#496)

--- app/components/DailyDeckRedirect/DailyDeckRedirect.tsx
+++ app/components/DailyDeckRedirect/DailyDeckRedirect.tsx
@@ -5,10 +5,20 @@ import { redirect } from "next/navigation";
 export const DailyDeckRedirect = async () => {
   const dailyDeck = await getDailyDeck();
   const headersList = headers();
-  const pathname = headersList.get('x-invoke-path') || '';
+  const pathname = headersList.get("x-path") || "";
 
-  if (dailyDeck && dailyDeck.questions.length > 0 && dailyDeck?.questions.every((q) => q.status === undefined) &&
-    pathname !== "/daily-deck") {
+  // enables direct link to the deck without redirecting to the daily deck
+  if (pathname.includes("/decks")) {
+    console.log('pathname includes "/decks"');
+    return null;
+  }
+
+  if (
+    dailyDeck &&
+    dailyDeck.questions.length > 0 &&
+    dailyDeck?.questions.every((q) => q.status === undefined) &&
+    pathname !== "/daily-deck"
+  ) {
     redirect("/daily-deck");
   }
 
