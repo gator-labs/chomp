bfdddb404f46bc3e401d3d7ec53ead381b5b25d3
Yash Garg <yash431garg@gmail.com>
2024-09-19 17:11:09 +0530

    feat: added loading spinner and disabled functionality in reveal button.

--- app/components/ui/button.tsx
+++ app/components/ui/button.tsx
@@ -1,8 +1,8 @@
 import { Slot } from "@radix-ui/react-slot";
 import { cva, type VariantProps } from "class-variance-authority";
 import * as React from "react";
-
 import { cn } from "@/lib/utils";
+import { SpinnerIcon } from "../Icons/ToastIcons/SpinnerIcon";
 
 const buttonVariants = cva(
   "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-neutral-950 disabled:pointer-events-none disabled:opacity-50 dark:focus-visible:ring-neutral-300",
@@ -29,22 +29,31 @@ const buttonVariants = cva(
 
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
-    VariantProps<typeof buttonVariants> {
+  VariantProps<typeof buttonVariants> {
   asChild?: boolean;
+  isLoading?: boolean;
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, asChild = false, ...props }, ref) => {
+  ({ className, variant, size, asChild = false, isLoading = false, children, ...props }, ref) => {
     const Comp = asChild ? Slot : "button";
     return (
       <Comp
-        className={cn(buttonVariants({ variant, size, className }))}
+        className={cn(buttonVariants({ variant, size, className }), isLoading && "cursor-not-allowed opacity-75")}
         ref={ref}
+        disabled={isLoading || props.disabled} // Disable the button if loading
         {...props}
-      />
+      >
+        {isLoading ? (
+          <SpinnerIcon />
+        ) : (
+          children
+        )}
+      </Comp>
     );
-  },
+  }
 );
 Button.displayName = "Button";
+
 
 export { Button, buttonVariants };
