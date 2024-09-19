772d78ae78bb825a47dd576d56f94919c6e54147
Marvin Arnold <marvin@gator.so>
2024-09-19 00:30:34 -0500

    Update app/components/LoginPopUp/LoginPopUp.tsx
    Co-authored-by: Yihwan Kim <Yihwan@users.noreply.github.com>

--- app/components/LoginPopUp/LoginPopUp.tsx
+++ app/components/LoginPopUp/LoginPopUp.tsx
@@ -55,7 +55,7 @@ const LoginPopUp = ({ isOpen, onClose, userId }: LoginPopUpProps) => {
           </div>
         </div>
         <p className="text-sm mb-6">
-          Get started by connecting wallet to begin answering questions.
+          Connect your wallet to answer questions and win prizes!
         </p>
 
         <DynamicConnectButton
