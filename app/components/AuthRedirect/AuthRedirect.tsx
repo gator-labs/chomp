ab7c096394ae38bdec60a2e42a2c0ec8bfbf7251
AndrejRadomiljac <andrej.web3@gmail.com>
2024-09-19 09:21:16 +0200

    PROD-313 added auth to admin routes

--- app/components/AuthRedirect/AuthRedirect.tsx
+++ app/components/AuthRedirect/AuthRedirect.tsx
@@ -1,4 +1,5 @@
 import { getJwtPayload } from "@/app/actions/jwt";
+import { getCurrentUser } from "@/app/queries/user";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 
@@ -13,5 +14,12 @@ export const AuthRedirect = async () => {
     redirect(`/login?next=${encodeURIComponent(path)}`);
   }
 
+  if (jwt && path?.includes("/admin")) {
+    const currentUser = await getCurrentUser();
+    if (currentUser?.isAdmin) {
+      return null;
+    }
+    redirect("/application");
+  }
   return null;
 };
