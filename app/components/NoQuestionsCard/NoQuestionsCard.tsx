87b38271c89efd1f1445580e9d5f7ab4f5cc373d
Neel <neelp0980@gmail.com>
2024-09-20 14:29:23 +0800

    feat: taskon mixpanel event

--- app/components/NoQuestionsCard/NoQuestionsCard.tsx
+++ app/components/NoQuestionsCard/NoQuestionsCard.tsx
@@ -1,19 +1,21 @@
 "use client";
 
+import { MIX_PANEL_EVENTS } from "@/app/constants/mixpanel";
+import sendToMixpanel from "@/lib/mixpanel";
+import gatorHeadImage from "@/public/images/gator-head.png";
+import { CircleArrowRight, Share2 } from "lucide-react";
+import { useRouter } from "next-nprogress-bar";
+import Image from "next/image";
 import { Button } from "../ui/button";
 import { QUESTION_CARD_CONTENT } from "./constants";
-import { useRouter } from "next-nprogress-bar";
-import { Share2, CircleArrowRight } from 'lucide-react';
-import Image from "next/image";
-import gatorHeadImage from "@/public/images/gator-head.png";
 
 type NoQuestionsCardProps = {
   variant:
-  | "daily-deck"
-  | "regular-deck"
-  | "answer-page"
-  | "answered-none"
-  | "answered-some";
+    | "daily-deck"
+    | "regular-deck"
+    | "answer-page"
+    | "answered-none"
+    | "answered-some";
   nextDeckId?: number;
   deckRevealAtDate?: Date | null;
 };
@@ -27,9 +29,7 @@ export function NoQuestionsCard({
 
   return (
     <div className="flex flex-col justify-between h-full w-full gap-4">
-      <div
-        className="questions-card text-white  relative mb-[4px]"
-      >
+      <div className="questions-card text-white  relative mb-[4px]">
         <div className="flex items-center justify-start text-left flex-col space-y-5">
           <div className="text-[24px] font-bold w-full text-purple-200">
             {QUESTION_CARD_CONTENT[variant].title}
@@ -47,38 +47,38 @@ export function NoQuestionsCard({
         />
       </div>
       <Button
-        onClick={() => window.open(process.env.NEXT_PUBLIC_REWARD_TASKON_URL, '_blank')}
+        onClick={() => {
+          sendToMixpanel(MIX_PANEL_EVENTS.SHARE_EARN_URL);
+          window.open(process.env.NEXT_PUBLIC_REWARD_TASKON_URL, "_blank");
+        }}
         className="text-[14px] gap-2"
       >
         Share & Earn More
         <Share2 />
       </Button>
-      {
-        nextDeckId ? (
-          <Button
-            variant="outline"
-            className="text-[14px] gap-2"
-            onClick={() => {
-              router.replace(`/application/decks/${nextDeckId}`);
-              router.refresh();
-
-            }}
-          >
-            Next Deck <CircleArrowRight />
-          </Button>
-        ) : (
-          <Button
-            variant="outline"
-            className="text-[14px] gap-2"
-            onClick={() => {
-              router.replace("/application");
-              router.refresh();
-            }}
-          >
-            Go Home   <CircleArrowRight />
-          </Button>
-        )
-      }
-    </div >
+      {nextDeckId ? (
+        <Button
+          variant="outline"
+          className="text-[14px] gap-2"
+          onClick={() => {
+            router.replace(`/application/decks/${nextDeckId}`);
+            router.refresh();
+          }}
+        >
+          Next Deck <CircleArrowRight />
+        </Button>
+      ) : (
+        <Button
+          variant="outline"
+          className="text-[14px] gap-2"
+          onClick={() => {
+            router.replace("/application");
+            router.refresh();
+          }}
+        >
+          Go Home <CircleArrowRight />
+        </Button>
+      )}
+    </div>
   );
 }
