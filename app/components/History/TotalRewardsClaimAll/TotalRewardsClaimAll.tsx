643a6ebd3868af8ce575a026527024a07afa65fe
Yash Garg <yash431garg@gmail.com>
2024-09-20 14:42:34 +0530

    feat: claim share modal functionality updates.

--- app/components/History/TotalRewardsClaimAll/TotalRewardsClaimAll.tsx
+++ app/components/History/TotalRewardsClaimAll/TotalRewardsClaimAll.tsx
@@ -12,8 +12,14 @@ import { numberToCurrencyFormatter } from "@/app/utils/currency";
 import sendToMixpanel from "@/lib/mixpanel";
 import { Question } from "@prisma/client";
 import { useQueryClient } from "@tanstack/react-query";
-import { startTransition, useOptimistic } from "react";
 import { Button } from "../../Button/Button";
+import { startTransition, useOptimistic, useState } from "react";
+import { Drawer, DrawerContent } from "../../ui/drawer";
+import { Button as SdButton } from "../../ui/button";
+import { CloseIcon } from "../../Icons/CloseIcon";
+import { ArrowDownToLine } from 'lucide-react';
+import { TwitterIcon } from "@/app/components/Icons/TwitterIcon"
+import Image from "next/image";
 
 type TotalRewardsClaimAllProps = {
   totalClaimableRewards?: {
@@ -21,6 +27,13 @@ type TotalRewardsClaimAllProps = {
     totalClaimableRewards: number;
   };
 };
+
+type ClaimQuestionData = {
+  questionIds: number[];
+  claimedAmount: number;
+  burnTx: string | null;
+}
+
 
 export default function TotalRewardsClaimAll({
   totalClaimableRewards,
@@ -33,6 +46,8 @@ export default function TotalRewardsClaimAll({
   const { fire } = useConfetti();
   const queryClient = useQueryClient();
   const { isClaiming, setIsClaiming } = useClaiming();
+  const [openClaimShareModal, setOpenClaimShareModal] = useState(false)
+  const [claimRes, setClaimRes] = useState<ClaimQuestionData>()
 
   const onClaimAll = async () => {
     try {
@@ -52,6 +67,11 @@ export default function TotalRewardsClaimAll({
         success: "Funds are transferred!",
         error: "Issue transferring funds.",
       });
+      setClaimRes({
+        questionIds: res?.questionIds ?? [],
+        claimedAmount: res?.claimedAmount ?? 0,
+        burnTx: res?.burnTx ?? null,
+      })
 
       sendToMixpanel(MIX_PANEL_EVENTS.CLAIM_SUCCEEDED, {
         [MIX_PANEL_METADATA.QUESTION_ID]: res?.questionIds,
@@ -72,6 +92,7 @@ export default function TotalRewardsClaimAll({
         `You have successfully claimed ${numberToCurrencyFormatter.format(totalClaimableRewards?.totalClaimableRewards || 0)} BONK!`,
       );
       setIsClaiming(false);
+      setOpenClaimShareModal(true);
     } catch (error) {
       sendToMixpanel(MIX_PANEL_EVENTS.CLAIM_FAILED, {
         [MIX_PANEL_METADATA.QUESTION_ID]: totalClaimableRewards?.questions.map(
@@ -81,9 +102,28 @@ export default function TotalRewardsClaimAll({
           totalClaimableRewards?.questions.map((q) => q?.question),
         [MIX_PANEL_METADATA.REVEAL_TYPE]: REVEAL_TYPE.ALL,
       });
+      setOpenClaimShareModal(false);
     } finally {
       setIsClaiming(false);
     }
+  };
+
+  const handleTwitterShare = () => {
+    const wonAmount = claimRes?.claimedAmount || 0;
+    const numCards = claimRes?.questionIds.length || 0;
+
+    const cardText = numCards === 1 ? "card" : "cards";
+
+    let tweetText = "";
+    if (wonAmount > 0) {
+      tweetText = `I just won ${wonAmount} $BONK for chomping ${numCards} ${cardText}. #chompchomp`;
+    } else {
+      tweetText = `I suck, do you? #chompchomp`;
+    }
+
+    const twitterUrl = `https://x.com/intent/post?url=https%3A%2F%2Fapp.chomp.games&text=${encodeURIComponent(tweetText)}&hashtags=chompchomp&via=chompdotgames`;
+
+    window.open(twitterUrl, '_blank');
   };
 
   return (
@@ -106,6 +146,61 @@ export default function TotalRewardsClaimAll({
           Claim all
         </Button>
       )}
+      <Drawer
+        open={openClaimShareModal}
+        onOpenChange={(open: boolean) => {
+          if (!open) {
+            setOpenClaimShareModal(false);
+          }
+        }}
+      >
+        <DrawerContent>
+          <SdButton
+            variant="ghost"
+            onClick={() => {
+              setOpenClaimShareModal(false);
+            }}
+            className="absolute top-5 right-6 border-none w-max !p-0 z-50"
+          >
+            <CloseIcon width={16} height={16} />
+          </SdButton>
+          <div className="flex flex-col gap-6 pt-6 px-6 pb-6">
+            <div className="flex flex-col gap-6">
+              <div className="flex flex-row w-full items-center justify-between">
+                <h3 className="text-secondary text-[16px]">
+                  Share your result!
+                </h3>
+              </div>
+              <p className="text-[14px]">
+                You just claimed ${claimRes?.claimedAmount} BONK from {claimRes?.questionIds.length} cards!
+              </p>
+            </div>
+
+            <div className="flex justify-center">
+              <Image
+                src={`${process.env.NEXT_PUBLIC_API_URL}/og/share-claim-all?burnTx=${'5Yb5nd57ZoHGXgK9eSMiCUQbGFCLaFxYWVq6SsccUdkhLKUqFpUBex4Fv8VqC69h9UC5FcYkCZNhx38GW6nz8ZcJ'}`}
+                alt="Claim All image"
+                width={100}
+                height={100}
+                className="p-4 w-[100%] h-auto"
+              />
+            </div>
+
+            <div className="flex flex-row px-5 justify-between">
+              <SdButton variant="ghost" className="bg-purple-200 text-gray-900 rounded-full w-[40px] h-[40px]">
+                <ArrowDownToLine />
+              </SdButton>
+              <SdButton
+                variant="ghost"
+                className="bg-purple-200 text-gray-900 rounded-full w-[40px] h-[40px]"
+                onClick={handleTwitterShare}
+              >
+                <TwitterIcon />
+              </SdButton>
+            </div>
+          </div>
+        </DrawerContent>
+      </Drawer>
     </div>
   );
 }
