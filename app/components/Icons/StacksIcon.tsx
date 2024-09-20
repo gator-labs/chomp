50212177070bcc536df76e7a9e454c5834d863a2
Danijel Radomiljac <danijel.radomiljac@blankhq.co>
2024-09-20 08:00:33 +0200

    add stacks page

---
+++ app/components/Icons/StacksIcon.tsx
@@ -0,0 +1,21 @@
+function StacksIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="23"
+      height="22"
+      fill="none"
+      viewBox="0 0 23 22"
+    >
+      <path
+        stroke="#B3B3B3"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        d="M5.077 8.5l-3.5 1.6a1 1 0 000 1.81l8.6 3.91a2.001 2.001 0 001.65 0l8.58-3.9a1 1 0 000-1.83l-3.5-1.59m-11.83 5l-3.5 1.6a1 1 0 000 1.81l8.6 3.91a2.001 2.001 0 001.65 0l8.58-3.9a1 1 0 000-1.83l-3.5-1.59m-5.08-12.32a2 2 0 00-1.66 0l-8.57 3.9a1 1 0 000 1.83l8.58 3.91a2.001 2.001 0 001.66 0l8.58-3.9a1 1 0 000-1.83l-8.59-3.91z"
+      ></path>
+    </svg>
+  );
+}
+
+export default StacksIcon;
