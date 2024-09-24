2aed3f34f12f3c5ce059ecf559f326f65f935013
AndrejRadomiljac <andrej.web3@gmail.com>
2024-09-24 09:48:39 +0200

    PROD-339 temporary added queries

--- app/queries/history.ts
+++ app/queries/history.ts
@@ -134,9 +134,9 @@ export async function getQuestionsHistoryQuery(
 `;
 
   let finalQuery = baseQuery;
-  if (process.env.NEXT_PUBLIC_ENVIRONMENT === "staging") {
-    finalQuery += havingClause;
-  }
+  // if (process.env.NEXT_PUBLIC_ENVIRONMENT === "staging") {
+  //   finalQuery += havingClause;
+  // }
   finalQuery += endQuery;
 
   const historyResult: QuestionHistory[] =
