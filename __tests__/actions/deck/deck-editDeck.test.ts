4ae4d9835584a77077d411904a575aa6f0db4f16
Yash Garg <yash431garg@gmail.com>
2024-10-02 19:49:16 +0530

    wip: skking edit title

--- __tests__/actions/deck/deck-editDeck.test.ts
+++ __tests__/actions/deck/deck-editDeck.test.ts
@@ -1,10 +1,13 @@
-import { editDeck } from "@/app/actions/deck/deck";
+import { deleteDeck, editDeck } from "@/app/actions/deck/deck";
 import { getDeckSchema } from "@/app/queries/deck";
 import { deckSchema } from "@/app/schemas/deck";
 import prisma from "@/app/services/prisma";
 import { ONE_MINUTE_IN_MILLISECONDS } from "@/app/utils/dateUtils";
+import { v4 as uuidv4 } from "uuid";
 import { z } from "zod";
 
+
+// Mocking Next.js dependencies and functions that are not the focus of this test
 jest.mock("@/app/queries/user", () => ({
   getIsUserAdmin: jest.fn(),
 }));
@@ -21,9 +24,13 @@ jest.mock("next/navigation", () => ({
   redirect: jest.fn(),
 }));
 
-describe.skip("Editing a Deck", () => {
+describe("Editing a Deck", () => {
   const currentDate = new Date();
+  let currentDeckId: number;
+  let currentDeckQuestionId: number;
+  let user: { id: string; username: string };
 
+  // Dummy deck data
   let mockData = {
     deck: `deck ${currentDate}`,
     description: "Description",
@@ -50,9 +57,7 @@ describe.skip("Editing a Deck", () => {
     imageUrl: "",
   } as unknown as z.infer<typeof deckSchema>;
 
-  let currentDeckId: number;
-  let currentDeckQuestionId: number;
-
+ // Add mock deck with one question and a user to database
   beforeAll(async () => {
     await prisma.$transaction(async (tx) => {
       const deck = await tx.deck.create({
@@ -69,41 +74,56 @@ describe.skip("Editing a Deck", () => {
           heading: mockData.heading,
         },
       });
-      const question  = mockData.questions[0]
-       const newQuestion = await tx.question.create({
-          data: {
-            question: question.question,
-            type: question.type,
-            imageUrl: question.imageUrl,
-            revealToken: mockData.revealToken,
-            revealTokenAmount: mockData.revealTokenAmount,
-            revealAtDate: mockData.revealAtDate,
-            revealAtAnswerCount: mockData.revealAtAnswerCount,
-            durationMiliseconds: ONE_MINUTE_IN_MILLISECONDS,
-            deckQuestions: {
-              create: {
-                deckId: deck.id,
-              },
+      const question = mockData.questions[0];
+      const newQuestion = await tx.question.create({
+        data: {
+          question: question.question,
+          type: question.type,
+          imageUrl: question.imageUrl,
+          revealToken: mockData.revealToken,
+          revealTokenAmount: mockData.revealTokenAmount,
+          revealAtDate: mockData.revealAtDate,
+          revealAtAnswerCount: mockData.revealAtAnswerCount,
+          durationMiliseconds: ONE_MINUTE_IN_MILLISECONDS,
+          deckQuestions: {
+            create: {
+              deckId: deck.id,
             },
-            questionOptions: {
-              createMany: {
-                data: question.questionOptions,
-              },
-            },
-            questionTags: {
-              createMany: {
-                data: mockData.tagIds.map((tagId) => ({ tagId })),
-              },
-            },
-            stackId: mockData.stackId,
           },
-        });
-      currentDeckQuestionId = newQuestion.id
+          questionOptions: {
+            createMany: {
+              data: question.questionOptions,
+            },
+          },
+          questionTags: {
+            createMany: {
+              data: mockData.tagIds.map((tagId) => ({ tagId })),
+            },
+          },
+          stackId: mockData.stackId,
+        },
+      });
+      currentDeckQuestionId = newQuestion.id;
       currentDeckId = deck.id;
+    });
+
+    user = {
+      id: uuidv4(),
+      username: `user1`,
+    };
+  });
+
+  // delete all the deck and user data after all the test run
+  afterAll(async () => {
+    deleteDeck(currentDeckId);
+    await prisma.user.delete({
+      where: {
+        id: user.id,
+      },
     });
   });
 
-  it("should edit deck tilte", async () => {
+  it.skip("should edit deck tilte", async () => {
     mockData = {
       id: currentDeckId,
       ...mockData,
@@ -124,7 +144,7 @@ describe.skip("Editing a Deck", () => {
           question: "Question",
           type: "MultiChoice",
           questionOptions: [
-            { option: "test", isCorrect: false, isLeft: false,  },
+            { option: "test", isCorrect: false, isLeft: false },
             { option: "test", isCorrect: false, isLeft: false },
             { option: "test", isCorrect: true, isLeft: false },
             { option: "test", isCorrect: false, isLeft: false },
@@ -135,7 +155,9 @@ describe.skip("Editing a Deck", () => {
     };
     await editDeck(mockData);
     const editedDeck = await getDeckSchema(currentDeckId);
-    expect(editedDeck?.questions[0].questionOptions).toMatchObject(mockData.questions[0].questionOptions);
+    expect(editedDeck?.questions[0].questionOptions).toMatchObject(
+      mockData.questions[0].questionOptions,
+    );
   });
 
   it("should delete a question from deck", async () => {
@@ -149,33 +171,67 @@ describe.skip("Editing a Deck", () => {
     expect(mockData?.questions).toEqual(editedDeck?.questions);
   });
 
-  // it("should not allow editing the deck after there is one or more answer", async () => {
-  //   mockData = {
-  //     id: currentDeckId,
-  //     ...mockData,
-  //     questions: [
-  //       {
-  //         question: "Question",
-  //         type: "MultiChoice",
-  //         questionOptions: [
-  //           { option: "1", isCorrect: false, isLeft: false },
-  //           { option: "2", isCorrect: false, isLeft: false },
-  //           { option: "3", isCorrect: true, isLeft: false },
-  //           { option: "4", isCorrect: false, isLeft: false },
-  //         ],
-  //         imageUrl: "",
-  //       },
-  //     ],
-  //   };
-  //   await editDeck(mockData);
-  //   const deck = await getDeckSchema(currentDeckId);
-  //   answerQuestion({
-  //     questionId: deck?.questions[0]?.id || 0,
-  //     questionOptionId: deck?.questions[0].questionOptions[0].id,
-  //     percentageGiven: 70,
-  //     percentageGivenForAnswerId: 51,
-  //     timeToAnswerInMiliseconds: 4000,
-  //     deckId: deck?.id
-  //   })
-  // });
+  it("should not allow editing the deck after there is one or more answer", async () => {
+    mockData = {
+      id: currentDeckId,
+      ...mockData,
+      questions: [
+        {
+          question: "Question",
+          type: "BinaryQuestion",
+          questionOptions: [
+            { option: "1", isCorrect: false, isLeft: true },
+            { option: "2", isCorrect: false, isLeft: false },
+          ],
+          imageUrl: "",
+        },
+      ],
+    };
+
+    await editDeck(mockData);
+    const editedDeck = await getDeckSchema(currentDeckId);
+
+    await prisma.user.create({
+      data: user,
+    });
+
+    const questionOptions = await prisma.questionOption.findMany({
+      where: {
+        question: {
+          deckQuestions: {
+            some: {
+              deckId: editedDeck?.id,
+            },
+          },
+        },
+      },
+    });
+
+    for (const option of questionOptions) {
+      // Random percentage for the first option
+      const randomPercentage = Math.floor(Math.random() * 100);
+      // Calculate the remaining percentage for the other option
+      const remainingPercentage = 100 - randomPercentage;
+
+      const selectedOption = questionOptions[0];
+
+      const isSelectedOption =
+        option.id ===
+        (selectedOption
+          ? selectedOption.id
+          : questionOptions[Math.floor(Math.random() * questionOptions.length)]
+              .id);
+
+      // Apply percentages such that they sum up to 100 for each user's answer set
+      await prisma.questionAnswer.create({
+        data: {
+          userId: user.id,
+          questionOptionId: option.id,
+          percentage: option.isLeft ? randomPercentage : remainingPercentage,
+          selected: isSelectedOption,
+          timeToAnswer: BigInt(Math.floor(Math.random() * 60000)), // Random time to answer within 60 seconds
+        },
+      });
+    }
+  });
 });
