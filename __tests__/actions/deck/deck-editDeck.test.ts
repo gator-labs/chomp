3bd214ff151017b4e01165abfdbcfa2173f9ae9c
Yash Garg <yash431garg@gmail.com>
2024-10-07 17:39:49 +0530

    updated test with image url

--- __tests__/actions/deck/deck-editDeck.test.ts
+++ __tests__/actions/deck/deck-editDeck.test.ts
@@ -42,7 +42,7 @@ describe("Editing a Deck", () => {
     durationMiliseconds: 10,
     questions: [
     ],
-    imageUrl: "",
+    imageUrl: "https://chomp-staging.s3.eu-north-1.amazonaws.com/fc01972b-adee-4ab6-855f-1f733cdc6428",
   } as unknown as z.infer<typeof deckSchema>;
 
  // Add mock deck with one question and a user to database
@@ -135,7 +135,7 @@ describe("Editing a Deck", () => {
     expect(updatedDeck!.deckQuestions[0].question.questionOptions).toHaveLength(2);
   });
 
-  it("should change a question a binary question to multi", async () => {
+  it("should change a question a binary question to multi with image", async () => {
     mockData = {
       id: currentDeckId,
       ...mockData,
@@ -150,7 +150,7 @@ describe("Editing a Deck", () => {
             { option: "test Option 3", isCorrect: true, isLeft: false },
             { option: "test Option 4", isCorrect: false, isLeft: false },
           ],
-          imageUrl: "",
+          imageUrl: "https://chomp-staging.s3.eu-north-1.amazonaws.com/12eb5d95-d1d6-456b-9dde-c3632ab71f7b",
         },
       ],
     };
