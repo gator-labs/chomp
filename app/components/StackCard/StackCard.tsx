47189e730b7688c4343ddf580f33fc9c74230dab
Danijel Radomiljac <danijel.radomiljac@blankhq.co>
2024-10-09 11:43:28 +0200

    add number of decks

--- app/components/StackCard/StackCard.tsx
+++ app/components/StackCard/StackCard.tsx
@@ -9,6 +9,7 @@ type StackCardProps = {
   id: number;
   imageSrc: string;
   name: string;
+  numberOfDecks: number;
   decksToAnswer?: number;
   decksToReveal?: number;
 };
@@ -19,10 +20,11 @@ const StackCard = ({
   name,
   decksToAnswer,
   decksToReveal,
+  numberOfDecks,
 }: StackCardProps) => {
   return (
     <Link
-      href={`${STACKS_PATH}/${id}`}
+      href={numberOfDecks > 0 ? `${STACKS_PATH}/${id}` : ""}
       className="p-4 rounded-[8px] bg-gray-800 border-[0.5px] border-solid border-gray-500 flex items-center justify-between gap-4"
       style={{
         pointerEvents:
@@ -39,10 +41,13 @@ const StackCard = ({
       </div>
       <div className="flex flex-col gap-3 flex-1">
         <h3 className="text-white text-sm font-bold">{name}</h3>
-        {decksToAnswer !== undefined && decksToReveal !== undefined ? (
-          decksToAnswer === 0 && decksToReveal === 0 ? (
-            <p className="text-xs font-medium text-gray-400">Coming soon</p>
-          ) : (
+        {numberOfDecks === 0 && (
+          <p className="text-xs font-medium text-gray-400">Coming soon</p>
+        )}
+
+        {decksToAnswer !== undefined &&
+          decksToReveal !== undefined &&
+          numberOfDecks > 0 && (
             <p className="text-xs font-medium text-gray-400">
               <span className="text-white">{decksToAnswer}</span> deck
               {decksToAnswer === 1 ? "" : "s"} to answer{" "}
@@ -50,8 +55,7 @@ const StackCard = ({
               <span className="text-white">{decksToReveal}</span> deck
               {decksToReveal === 1 ? "" : "s"} to reveal
             </p>
-          )
-        ) : null}
+          )}
       </div>
       {!(decksToAnswer === 0 && decksToReveal === 0) && (
         <div className="flex-shrink-0">
--- app/queries/stack.ts
+++ app/queries/stack.ts
@@ -136,8 +136,10 @@ export async function getAllStacks() {
 
   return stacks.map((stack) => ({
     ...stack,
+    decks: stack.deck,
     decksToAnswer: !!userId ? getDecksToAnswer(stack.deck) : undefined,
     decksToReveal: !!userId ? getDecksToReveal(stack.deck) : undefined,
+    deck: undefined,
   }));
 }
 
