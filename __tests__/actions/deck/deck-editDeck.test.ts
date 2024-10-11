e79d6c3aadfc854f74fd6ff6ab5f18072b7382f7
Yash Garg <yash431garg@gmail.com>
2024-10-11 08:30:02 +0530

    updated test for deck question updates

--- __tests__/actions/deck/deck-editDeck.test.ts
+++ __tests__/actions/deck/deck-editDeck.test.ts
@@ -2,7 +2,6 @@ import { deleteDeck, editDeck } from "@/app/actions/deck/deck";
 import { getDeckSchema } from "@/app/queries/deck";
 import { deckSchema } from "@/app/schemas/deck";
 import prisma from "@/app/services/prisma";
-import { ONE_MINUTE_IN_MILLISECONDS } from "@/app/utils/dateUtils";
 import { v4 as uuidv4 } from "uuid";
 import { z } from "zod";
 
@@ -24,7 +23,6 @@ jest.mock("next/navigation", () => ({
 describe("Editing a Deck", () => {
   const currentDate = new Date();
   let currentDeckId: number;
-  let currentDeckQuestionId: number;
   let user: { id: string; username: string };
 
   // Dummy deck data
@@ -40,8 +38,7 @@ describe("Editing a Deck", () => {
     revealTokenAmount: 50,
     revealAtAnswerCount: null,
     durationMiliseconds: 10,
-    questions: [
-    ],
+    questions: [],
     imageUrl: "",
   } as unknown as z.infer<typeof deckSchema>;
 
@@ -74,25 +71,32 @@ describe("Editing a Deck", () => {
   // delete all the deck and user data after all the test run
   afterAll(async () => {
     deleteDeck(currentDeckId);
-    // await prisma.user.delete({
-    //   where: {
-    //     id: user.id,
-    //   },
-    // });
+    await prisma.user.delete({
+      where: {
+        id: user.id,
+      },
+    });
   });
 
-  it.skip("should edit deck tilte", async () => {
+  it("should edit deck meta data", async () => {
     mockData = {
       id: currentDeckId,
       ...mockData,
       deck: `updated ${currentDate}`,
+      description: "Updated Description",
+      footer: "Updated Footer",
     };
     await editDeck(mockData);
     const editedDeck = await getDeckSchema(currentDeckId);
-    expect(mockData?.deck).toEqual(editedDeck?.deck);
+
+    // Test each field individually
+    expect(editedDeck?.deck).toEqual(mockData.deck);
+    expect(editedDeck?.description).toEqual(mockData.description);
+    expect(editedDeck?.footer).toEqual(mockData.footer);
+    expect(editedDeck?.imageUrl).toEqual(mockData.imageUrl);
   });
 
-  it('should successfully add new questions to an existing deck', async () => {
+  it("should successfully add new questions to an existing deck", async () => {
     mockData = {
       id: currentDeckId,
       ...mockData,
@@ -131,17 +135,20 @@ describe("Editing a Deck", () => {
     expect(updatedDeck!.deck).toBe(mockData.deck);
     expect(updatedDeck!.deckQuestions).toHaveLength(1);
     expect(updatedDeck!.deckQuestions).toHaveLength(1);
-    expect(updatedDeck!.deckQuestions[0].question.question).toBe('New Test Question');
-    expect(updatedDeck!.deckQuestions[0].question.questionOptions).toHaveLength(2);
+    expect(updatedDeck!.deckQuestions[0].question.question).toBe(
+      "New Test Question",
+    );
+    expect(updatedDeck!.deckQuestions[0].question.questionOptions).toHaveLength(
+      2,
+    );
   });
 
-  it("should change a question a binary question to multi", async () => {
+  it("Should update the previously added binary question to a multichoice question.", async () => {
     mockData = {
       id: currentDeckId,
       ...mockData,
       questions: [
         {
-          id: currentDeckQuestionId,
           question: "Updated Question",
           type: "MultiChoice",
           questionOptions: [
@@ -156,9 +163,11 @@ describe("Editing a Deck", () => {
     };
     await editDeck(mockData);
     const editedDeck = await getDeckSchema(currentDeckId);
-    expect(editedDeck!.questions[0].question).toBe('Updated Question');
+    expect(editedDeck!.questions[0].question).toBe("Updated Question");
     expect(editedDeck!.questions[0].questionOptions).toHaveLength(4);
-    expect(editedDeck!.questions[0].questionOptions[0].option).toBe('test Option 1');
+    expect(editedDeck!.questions[0].questionOptions[0].option).toBe(
+      "test Option 1",
+    );
   });
 
   it("should delete a question from deck", async () => {
@@ -172,80 +181,136 @@ describe("Editing a Deck", () => {
     expect(mockData?.questions).toEqual(editedDeck?.questions);
   });
 
-  it.skip("should not allow editing the deck after there is one or more answer", async () => {
-    mockData = {
-      id: currentDeckId,
-      ...mockData,
-      questions: [
-        {
-          question: "Question",
-          type: "BinaryQuestion",
-          questionOptions: [
-            { option: "1", isCorrect: false, isLeft: true },
-            { option: "2", isCorrect: false, isLeft: false },
-          ],
-          imageUrl: "",
-        },
-      ],
-    };
+  describe("testing answered deck", () => {
+    let questionId: number;
+    let optionId1: number;
+    let optionId2: number;
 
-    await editDeck(mockData);
-    const editedDeck = await getDeckSchema(currentDeckId);
+    // Add a question to the deck
+    beforeAll(async () => {
+      mockData = {
+        id: currentDeckId,
+        ...mockData,
+        questions: [
+          {
+            question: "New Question",
+            type: "BinaryQuestion",
+            questionOptions: [
+              { option: "1", isCorrect: false, isLeft: true },
+              { option: "2", isCorrect: false, isLeft: false },
+            ],
+            imageUrl: "",
+          },
+        ],
+      };
 
-    await prisma.user.create({
-      data: user,
-    });
+      await editDeck(mockData);
+      const editedDeck = await getDeckSchema(currentDeckId);
+      if(editedDeck){
+        questionId = editedDeck?.questions[0]?.id
+        optionId1 = editedDeck?.questions[0].questionOptions[0].id
+        optionId2 = editedDeck?.questions[0].questionOptions[1].id
+      }
 
-    const questionOptions = await prisma.questionOption.findMany({
-      where: {
-        question: {
-          deckQuestions: {
-            some: {
-              deckId: editedDeck?.id,
+      // create a new user
+      await prisma.user.create({
+        data: user,
+      });
+
+      //add answer fo the question
+      const questionOptions = await prisma.questionOption.findMany({
+        where: {
+          question: {
+            deckQuestions: {
+              some: {
+                deckId: editedDeck?.id,
+              },
             },
           },
         },
-      },
+      });
+
+      for (const option of questionOptions) {
+        // Random percentage for the first option
+        const randomPercentage = Math.floor(Math.random() * 100);
+        // Calculate the remaining percentage for the other option
+        const remainingPercentage = 100 - randomPercentage;
+
+        const selectedOption = questionOptions[0];
+
+        const isSelectedOption =
+          option.id ===
+          (selectedOption
+            ? selectedOption.id
+            : questionOptions[
+                Math.floor(Math.random() * questionOptions.length)
+              ].id);
+
+        // Apply percentages such that they sum up to 100 for each user's answer set
+        await prisma.questionAnswer.create({
+          data: {
+            userId: user.id,
+            questionOptionId: option.id,
+            percentage: option.isLeft ? randomPercentage : remainingPercentage,
+            selected: isSelectedOption,
+            timeToAnswer: BigInt(Math.floor(Math.random() * 60000)), // Random time to answer within 60 seconds
+          },
+        });
+      }
     });
 
-    for (const option of questionOptions) {
-      // Random percentage for the first option
-      const randomPercentage = Math.floor(Math.random() * 100);
-      // Calculate the remaining percentage for the other option
-      const remainingPercentage = 100 - randomPercentage;
+    it("should allow editing the deck and question metadata after there is one or more answer", async () => {
+      mockData = {
+        id: currentDeckId,
+        ...mockData,
+        deck: "Updated Test Deck",
+        description: "Updated Description",
+        footer: "Updated Footer",
+        questions: [
+          {
+            id: questionId,
+            question: "Updated Question After Answer",
+            type: "BinaryQuestion",
+            questionOptions: [
+              { id: optionId1, option: "1", isCorrect: false, isLeft: true },
+              { id: optionId2, option: "2", isCorrect: false, isLeft: false },
+            ],
+            imageUrl: "",
+          },
+        ],
+      };
 
-      const selectedOption = questionOptions[0];
+      await editDeck(mockData);
+      const editedDeck = await getDeckSchema(currentDeckId);
 
-      const isSelectedOption =
-        option.id ===
-        (selectedOption
-          ? selectedOption.id
-          : questionOptions[Math.floor(Math.random() * questionOptions.length)]
-              .id);
+      expect(editedDeck!.questions[0].question).toBe("Updated Question After Answer");
+      expect(editedDeck?.deck).toEqual(mockData.deck);
+      expect(editedDeck?.description).toEqual(mockData.description);
+      expect(editedDeck?.footer).toEqual(mockData.footer);
+      expect(editedDeck?.imageUrl).toEqual(mockData.imageUrl);
+    });
 
-      // Apply percentages such that they sum up to 100 for each user's answer set
-      await prisma.questionAnswer.create({
-        data: {
-          userId: user.id,
-          questionOptionId: option.id,
-          percentage: option.isLeft ? randomPercentage : remainingPercentage,
-          selected: isSelectedOption,
-          timeToAnswer: BigInt(Math.floor(Math.random() * 60000)), // Random time to answer within 60 seconds
-        },
-      });
-    }
-
-    const updateData = {
-      ...mockData,
-      deck: 'Updated Test Deck',
-    };
-
-    const result = await editDeck(updateData);
-
-    expect(result).toEqual({ errorMessage: 'Cannot edit deck' });
-
-    const unchangedDeck = await getDeckSchema(currentDeckId);
-
-    expect(unchangedDeck!.deck).toBe(mockData.deck);
+    it("Should update the previously added binary question to a multichoice question.", async () => {
+      mockData = {
+        id: currentDeckId,
+        ...mockData,
+        questions: [
+          {
+            id: questionId,
+            question: "Updated Question",
+            type: "MultiChoice",
+            questionOptions: [
+              { option: "test Option 1", isCorrect: false, isLeft: false },
+              { option: "test Option 2", isCorrect: false, isLeft: false },
+              { option: "test Option 3", isCorrect: true, isLeft: false },
+              { option: "test Option 4", isCorrect: false, isLeft: false },
+            ],
+            imageUrl: "",
+          },
+        ],
+      };
+      const result = await editDeck(mockData);
+      expect(result).toEqual({ errorMessage: "Question type can't be changed if there's an answer." });
+    });
   });
 });
