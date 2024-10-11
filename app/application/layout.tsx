64372719d69aacb3f3d7ea53f851716552243ca5
Danijel Radomiljac <danijel.radomiljac@blankhq.co>
2024-10-11 10:44:36 +0200

    nav fix

--- app/application/layout.tsx
+++ app/application/layout.tsx
@@ -45,9 +45,9 @@ export default async function Layout({ children }: PageLayoutProps) {
                 }))}
                 address={address}
               />
-              <div className="flex-grow">{children}</div>
+              <div className="flex-grow pb-20">{children}</div>
             </Main>
-            <div className="sticky bottom-0 left-0 right-0 z-50">
+            <div className="fixed bottom-0 left-0 right-0 z-50">
               <TabNavigation isAdmin={!!user?.isAdmin} />
             </div>
             <AuthRedirect />
