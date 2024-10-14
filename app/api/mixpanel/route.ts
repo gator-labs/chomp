d08c1ba3dfa53822b02a131ee493c66b2beb5e19
Neel <neelp0980@gmail.com>
2024-10-14 16:52:51 +0530

    chore[mixpanel]: add initial and last utm tracking

--- app/api/mixpanel/route.ts
+++ app/api/mixpanel/route.ts
@@ -2,10 +2,16 @@ import { NextRequest, NextResponse, userAgent } from "next/server";
 
 import { TRACKING_METADATA } from "@/app/constants/tracking";
 import { getCurrentUser } from "@/app/queries/user";
+import { kv } from "@/lib/kv";
 import Mixpanel from "mixpanel";
 import { ReadonlyHeaders } from "next/dist/server/web/spec-extension/adapters/headers";
 
 const mixpanel = Mixpanel.init(process.env.MIX_PANEL_TOKEN!);
+
+interface UtmData {
+  initial_utm?: Record<string, string>;
+  last_utm?: Record<string, string>;
+}
 
 function getIPAddress(headersList: ReadonlyHeaders) {
   const FALLBACK_IP_ADDRESS = "0.0.0.0";
@@ -23,7 +29,6 @@ export async function POST(request: NextRequest) {
 
   try {
     const currentUser = await getCurrentUser();
-    
     const { event, properties } = data;
     const ip = getIPAddress(request.headers);
     const { device, browser, os } = userAgent({ headers: request.headers });
@@ -39,8 +44,54 @@ export async function POST(request: NextRequest) {
         ip,
       });
     } else {
+      const utmParams = {
+        utm_source: properties.$utm_source,
+        utm_medium: properties.$utm_medium,
+        utm_campaign: properties.$utm_campaign,
+        utm_term: properties.$utm_term,
+        utm_content: properties.$utm_content,
+      };
+
+      const {
+        $utm_source,
+        $utm_medium,
+        $utm_campaign,
+        $utm_term,
+        $utm_content,
+        ...propertiesWithoutUtm
+      } = properties;
+
+      // Get stored UTM data from KV
+      const storedUtmData = (await kv.get(
+        `utm:${currentUser.id}`,
+      )) as UtmData | null;
+      let initialUtm = storedUtmData?.initial_utm || {};
+      let lastUtm = storedUtmData?.last_utm || {};
+
+      // Check if any new UTM parameters are provided
+      const hasNewUtmParams = Object.values(utmParams).some(
+        (value) => value !== undefined,
+      );
+
+      // Update initial and last UTM data
+      if (!storedUtmData || hasNewUtmParams) {
+        initialUtm = initialUtm.utm_source ? initialUtm : utmParams;
+        lastUtm = { ...lastUtm, ...utmParams };
+
+        // Remove undefined values
+        Object.keys(lastUtm).forEach(
+          (key) => lastUtm[key] === undefined && delete lastUtm[key],
+        );
+
+        // Store updated UTM data in KV
+        await kv.set(`utm:${currentUser.id}`, {
+          initial_utm: initialUtm,
+          last_utm: lastUtm,
+        });
+      }
+
       mixpanel.track(event, {
-        ...properties,
+        ...propertiesWithoutUtm,
         $device: device.model,
         $browser: browser.name,
         $os: os.name,
@@ -48,12 +99,20 @@ export async function POST(request: NextRequest) {
         $browser_version: browser.version,
         [TRACKING_METADATA.USER_ID]: currentUser.id,
         [TRACKING_METADATA.USERNAME]: currentUser.username,
-        [TRACKING_METADATA.USER_WALLET_ADDRESS]:
-          currentUser.wallets[0].address,
+        [TRACKING_METADATA.USER_WALLET_ADDRESS]: currentUser.wallets[0].address,
+        initial_utm_source: initialUtm.utm_source,
+        initial_utm_medium: initialUtm.utm_medium,
+        initial_utm_campaign: initialUtm.utm_campaign,
+        initial_utm_term: initialUtm.utm_term,
+        initial_utm_content: initialUtm.utm_content,
+        last_utm_source: lastUtm.utm_source,
+        last_utm_medium: lastUtm.utm_medium,
+        last_utm_campaign: lastUtm.utm_campaign,
+        last_utm_term: lastUtm.utm_term,
+        last_utm_content: lastUtm.utm_content,
         ip,
       });
     }
-
 
     return NextResponse.json({ status: "Event tracked successfully" });
   } catch (error) {
