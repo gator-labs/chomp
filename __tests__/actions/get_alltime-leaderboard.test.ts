32338c39e945684ef69c9c25a8a506a2ee1591dd
Yash Garg <yash431garg@gmail.com>
2024-10-16 11:37:10 +0530

    added mixpanel env in test workflow and update time to 30s

--- __tests__/actions/get_alltime-leaderboard.test.ts
+++ __tests__/actions/get_alltime-leaderboard.test.ts
@@ -20,7 +20,7 @@ jest.mock("next/cache", () => ({
   revalidatePath: jest.fn(),
 }));
 
-jest.setTimeout(60000);
+jest.setTimeout(30000);
 
 describe("Get All-time leaderboard data", () => {
   const currentDate = new Date();
