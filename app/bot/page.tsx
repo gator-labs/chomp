ecf95487b2da96bc32ade3c9dbde1793f553d345
Marvin Arnold <marvin@geeky.rocks>
2024-10-17 22:57:04 -0500

    feat: add burn button

--- app/bot/page.tsx
+++ app/bot/page.tsx
@@ -4,9 +4,14 @@
 import { DynamicWidget, useDynamicContext, useTelegramLogin } from "@dynamic-labs/sdk-react-core";
 import { useEffect, useState } from "react";
 import Spinner from "./Spinner";
+import { ISolana, isSolanaWallet } from '@dynamic-labs/solana-core';
+import { genBonkBurnTx } from "../utils/solana";
+import { Connection } from '@solana/web3.js';
+
+const BONK_AMOUNT = 5
 
 export default function Main() {
-  const { sdkHasLoaded, user } = useDynamicContext();
+  const { sdkHasLoaded, user, primaryWallet } = useDynamicContext();
   const { telegramSignIn } = useTelegramLogin();
   const [isLoading, setIsLoading] = useState<boolean>(true);
 
@@ -25,6 +30,26 @@ export default function Main() {
     signIn();
   }, [sdkHasLoaded]);
 
+
+  const burnBonk = async () => {
+    if(!primaryWallet || !isSolanaWallet(primaryWallet)) {
+        return;
+      }
+      
+    const connection: Connection = await primaryWallet.getConnection();
+    const blockhash = await connection.getLatestBlockhash();
+
+    const signer = await primaryWallet.getSigner()
+
+    const tx = await genBonkBurnTx(
+        primaryWallet.address,
+        blockhash.blockhash,
+        BONK_AMOUNT,
+    );
+    
+    signer.signAndSendTransaction(tx)
+  }
+
   return (
     <div className="min-h-screen flex flex-col items-center justify-center text-black" style={{backgroundColor: "#f9f9fb", backgroundImage: "url('/background-pattern.svg')", backgroundBlendMode: "overlay", backgroundRepeat: "repeat"}}>
       <div className="flex flex-col items-center justify-center text-center max-w-3xl px-4">
@@ -35,23 +60,14 @@ export default function Main() {
         </div>
 
         <div className="bg-white p-5 rounded-lg shadow-sm mb-7 mt-7 text-sm">
-          <h2 className="text-xl font-semibold mb-3">You got an auto-wallet!</h2>
+          <h2 className="text-xl font-semibold mb-3">Start chomping</h2>
           <div className="flex justify-center py-4">
             {isLoading ? <Spinner /> : <DynamicWidget />}
           </div>
-          <p className="mb-3">
-            Zero clicks, one multi-chain wallet. We automatically created an embedded wallet for you.
+          {user && (<><p className="mb-3">
+            Zero clicks, your wallet already exists.
           </p>
-          <h3 className="text-lg font-semibold mb-2">How This works</h3>
-          <ul className="list-disc list-inside mb-3 flex flex-col items-start">
-            <li>We utilize the Telegram authentication token</li>
-            <li>Token is verified and used to create the end user wallet</li>
-            <li>The same wallet is accessible on desktop and mobile platforms</li>
-            <li>If the end user logs in with Telegram later on your site, your wallet is still available</li>
-          </ul>
-          <a href="https://docs.dynamic.xyz/guides/integrations/telegram/telegram-auto-wallets" target="_blank" rel="noopener noreferrer" className="mt-3 inline-block bg-blue-600 text-white font-semibold py-1.5 px-3 rounded hover:bg-blue-700 transition duration-300 text-sm">
-            Learn More in Our Docs
-          </a>
+          <button className='bg-purple-500 p-2 rounded-md text-lg' onClick={burnBonk}>Burn {BONK_AMOUNT} $BONK</button></>)}
         </div>
       </div>
     </div>
