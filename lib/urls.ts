470df51b3fc1a4fc6f3055091b5dbf94f93a2392
Danijel Radomiljac <danijel.radomiljac@blankhq.co>
2024-10-23 14:02:33 +0200

    rename claim page

--- lib/urls.ts
+++ lib/urls.ts
@@ -1,11 +1,11 @@
-import { encrypt } from '@/app/utils/crypto-js';
+import { encrypt } from "@/app/utils/crypto-js";
 
-export const HOME_PATH = '/application';
-export const ADMIN_PATH = '/admin';
+export const HOME_PATH = "/application";
+export const ADMIN_PATH = "/admin";
 export const HISTORY_PATH = `${HOME_PATH}/history`;
 export const getDeckPath = (deckId: string | number) =>
   `${HOME_PATH}/decks/${deckId.toString()}`;
-export const STACKS_PATH = '/stacks';
+export const STACKS_PATH = "/stacks";
 export const LEADERBOARD_PATH = `${HOME_PATH}/leaderboard`;
 export const ANSWER_PATH = `${HOME_PATH}/answer`;
 export const getOgShareClaimAllPath = (txHash: string) =>
@@ -13,5 +13,5 @@ export const getOgShareClaimAllPath = (txHash: string) =>
 export const getClaimAllShareUrl = (txHash: string) => {
   const encryptedTxHash = encrypt(txHash);
 
-  return `${process.env.NEXT_PUBLIC_APP_URL}/claim/${encryptedTxHash}`;
+  return `${process.env.NEXT_PUBLIC_APP_URL}/a/${encryptedTxHash}`;
 };
