5f9453ecb7516a7e0d510e81fcf459df7cbff4b5
Marvin Arnold <marvin@geeky.rocks>
2024-11-12 14:56:59 -0600

    fix: prettier

--- __tests__/queries/history/sql-injection-regression.ts
+++ __tests__/queries/history/sql-injection-regression.ts
@@ -1,15 +1,17 @@
-import { getQuestionsHistoryQuery } from '@/app/queries/history';
+import { getQuestionsHistoryQuery } from "@/app/queries/history";
 
 describe("SQL injection regression", () => {
-	it("should not allow deckId to interfere with query", async () => {
-		// We expect this to still throw an exception as the
-		// deckId needs to be an integer
-		await expect(getQuestionsHistoryQuery(
-			"11111111-1111-1111-1111-111111111111",
-			100,
-			1,
-			// @ts-expect-error
-			'\'',
-		)).rejects.not.toThrow(/unterminated quoted string/);
-	});
+  it("should not allow deckId to interfere with query", async () => {
+    // We expect this to still throw an exception as the
+    // deckId needs to be an integer
+    await expect(
+      getQuestionsHistoryQuery(
+        "11111111-1111-1111-1111-111111111111",
+        100,
+        1,
+        // @ts-expect-error prettier prefers this to ts-ignore
+        "'",
+      ),
+    ).rejects.not.toThrow(/unterminated quoted string/);
+  });
 });
