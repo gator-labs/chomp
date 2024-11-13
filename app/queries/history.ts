6f33a2b8f8b4bf9f96f4d438960724e1e422eaf0
Shaun Amott <shaun@inerd.com>
2024-11-13 22:18:37 +0100

    Add isRevealed filter to history query.

--- app/queries/history.ts
+++ app/queries/history.ts
@@ -31,6 +31,8 @@ export type QuestionHistory = {
   image?: string;
 };
 
+export type QuestionHistoryFilter = "isRevealable" | "all";
+
 export async function getDecksHistory(
   userId: string,
 ): Promise<HistoryResult[]> {
@@ -66,65 +68,70 @@ export async function getQuestionsHistoryQuery(
   pageSize: number,
   currentPage: number,
   deckId?: number,
+  filter: QuestionHistoryFilter = "all",
 ): Promise<QuestionHistory[]> {
   const offset = (currentPage - 1) * pageSize;
 
   const getAllDecks = !deckId;
 
   const historyResult: QuestionHistory[] = await prisma.$queryRaw`
-  SELECT 
-    q.id, 
-    q.question,
-    q."revealAtDate",
-    cr."rewardTokenAmount" as "claimedAmount",
-    cr."burnTransactionSignature",
-    c."image",
-    q."revealTokenAmount",
-    CASE 
-      WHEN COUNT(CASE WHEN qa.selected = true THEN 1 ELSE NULL END) > 0 THEN true
-      ELSE false 
-    END AS "isAnswered",
-    CASE 
-      WHEN COUNT(CASE WHEN cr.result = 'Claimed' AND cr."rewardTokenAmount" > 0 THEN 1 ELSE NULL END) > 0 THEN true
-      ELSE false 
-    END AS "isClaimed",
-    CASE 
-      WHEN COUNT(CASE WHEN (cr.result = 'Claimed' AND cr."rewardTokenAmount" > 0) OR (cr.result = 'Revealed' AND cr."transactionStatus" = 'Completed') THEN 1 ELSE NULL END) > 0 THEN true
-      ELSE false 
-    END AS "isRevealed",
-    CASE 
-      WHEN COUNT(CASE WHEN cr.result = 'Revealed' AND cr."rewardTokenAmount" > 0 THEN 1 ELSE NULL END) > 0
-          AND COUNT(CASE WHEN cr.result = 'Claimed' AND cr."rewardTokenAmount" > 0 THEN 1 ELSE NULL END) = 0 THEN true
-      ELSE false 
-    END AS "isClaimable",
-    CASE 
-      WHEN COUNT(CASE WHEN cr.result = 'Claimed' OR (cr.result = 'Revealed' AND cr."transactionStatus" = 'Completed') THEN 1 ELSE NULL END) = 0
-          AND q."revealAtDate" < NOW() THEN true
-      ELSE false 
-    END AS "isRevealable"
-  FROM 
-    public."Question" q 
-  JOIN 
-    public."QuestionOption" qo ON qo."questionId" = q.id 
-  LEFT JOIN 
-    public."QuestionAnswer" qa ON qa."questionOptionId" = qo.id AND qa."userId" = ${userId}
-  LEFT JOIN 
-    public."ChompResult" cr ON cr."questionId" = q.id AND cr."userId" = ${userId} AND cr."questionId" IS NOT NULL
-  FULL JOIN public."Stack" c on c.id = q."stackId"
-  JOIN public."DeckQuestion" dq ON dq."questionId" = q.id
-  WHERE 
-    q."revealAtDate" IS NOT NULL AND (${getAllDecks} IS TRUE OR dq."deckId" = ${deckId})
-  GROUP BY 
-    q.id, cr."rewardTokenAmount", cr."burnTransactionSignature", c."image"
-  HAVING 
-    (
-      SELECT COUNT(distinct concat(qa."userId", qo."questionId"))
-      FROM public."QuestionOption" qo
-      JOIN public."QuestionAnswer" qa ON qa."questionOptionId" = qo."id"
-      WHERE qo."questionId" = q."id"
-    ) >= ${Number(process.env.MINIMAL_ANSWERS_PER_QUESTION)}
-  ORDER BY q."revealAtDate" DESC, q."id"
-  LIMIT ${pageSize} OFFSET ${offset}
+  SELECT * FROM (
+      SELECT
+        q.id,
+        q.question,
+        q."revealAtDate",
+        cr."rewardTokenAmount" as "claimedAmount",
+        cr."burnTransactionSignature",
+        c."image",
+        q."revealTokenAmount",
+        CASE
+          WHEN COUNT(CASE WHEN qa.selected = true THEN 1 ELSE NULL END) > 0 THEN true
+          ELSE false
+        END AS "isAnswered",
+        CASE
+          WHEN COUNT(CASE WHEN cr.result = 'Claimed' AND cr."rewardTokenAmount" > 0 THEN 1 ELSE NULL END) > 0 THEN true
+          ELSE false
+        END AS "isClaimed",
+        CASE
+          WHEN COUNT(CASE WHEN (cr.result = 'Claimed' AND cr."rewardTokenAmount" > 0) OR (cr.result = 'Revealed' AND cr."transactionStatus" = 'Completed') THEN 1 ELSE NULL END) > 0 THEN true
+          ELSE false
+        END AS "isRevealed",
+        CASE
+          WHEN COUNT(CASE WHEN cr.result = 'Revealed' AND cr."rewardTokenAmount" > 0 THEN 1 ELSE NULL END) > 0
+              AND COUNT(CASE WHEN cr.result = 'Claimed' AND cr."rewardTokenAmount" > 0 THEN 1 ELSE NULL END) = 0 THEN true
+          ELSE false
+        END AS "isClaimable",
+        CASE
+          WHEN COUNT(CASE WHEN cr.result = 'Claimed' OR (cr.result = 'Revealed' AND cr."transactionStatus" = 'Completed') THEN 1 ELSE NULL END) = 0
+              AND q."revealAtDate" < NOW() THEN true
+          ELSE false
+        END AS "isRevealable"
+      FROM
+        public."Question" q
+      JOIN
+        public."QuestionOption" qo ON qo."questionId" = q.id
+      LEFT JOIN
+        public."QuestionAnswer" qa ON qa."questionOptionId" = qo.id AND qa."userId" = ${userId}
+      LEFT JOIN
+        public."ChompResult" cr ON cr."questionId" = q.id AND cr."userId" = ${userId} AND cr."questionId" IS NOT NULL
+      FULL JOIN public."Stack" c on c.id = q."stackId"
+      JOIN public."DeckQuestion" dq ON dq."questionId" = q.id
+      WHERE
+        q."revealAtDate" IS NOT NULL AND (${getAllDecks} IS TRUE OR dq."deckId" = ${deckId})
+      GROUP BY
+        q.id, cr."rewardTokenAmount", cr."burnTransactionSignature", c."image"
+      HAVING
+        (
+          SELECT COUNT(distinct concat(qa."userId", qo."questionId"))
+          FROM public."QuestionOption" qo
+          JOIN public."QuestionAnswer" qa ON qa."questionOptionId" = qo."id"
+          WHERE qo."questionId" = q."id"
+        ) >= ${Number(process.env.MINIMAL_ANSWERS_PER_QUESTION)}
+      ORDER BY q."revealAtDate" DESC, q."id"
+      LIMIT ${pageSize} OFFSET ${offset}
+  ) WHERE (
+      ${filter} = 'all' OR (${filter} = 'isRevealable' AND "isRevealable" IS true)
+  )
 `;
 
   return historyResult.map((hr) => ({
