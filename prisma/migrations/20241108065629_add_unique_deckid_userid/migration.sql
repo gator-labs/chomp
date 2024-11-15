eb91f9a83dc58600d4896a2b143e85aa437719c1
Yash Garg <yash431garg@gmail.com>
2024-11-15 14:58:14 +0530

    feat: updated constraints for fungibleAssetTransactionLog

--- prisma/migrations/20241108065629_add_unique_deckid_userid/migration.sql
+++ prisma/migrations/20241108065629_add_unique_deckid_userid/migration.sql
@@ -1,12 +1,12 @@
 /*
   Warnings:
 
-  - A unique constraint covering the columns `[userId,questionId]` on the table `FungibleAssetTransactionLog` will be added. If there are existing duplicate values, this will fail.
-  - A unique constraint covering the columns `[deckId,userId]` on the table `FungibleAssetTransactionLog` will be added. If there are existing duplicate values, this will fail.
+  - A unique constraint covering the columns `[type, userId,questionId]` on the table `FungibleAssetTransactionLog` will be added. If there are existing duplicate values, this will fail.
+  - A unique constraint covering the columns `[type, deckId,userId]` on the table `FungibleAssetTransactionLog` will be added. If there are existing duplicate values, this will fail.
 
 */
 -- CreateIndex
 CREATE UNIQUE INDEX "FungibleAssetTransactionLog_type_userId_questionId_key" ON "FungibleAssetTransactionLog"("type", "userId", "questionId");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "FungibleAssetTransactionLog_deckId_userId_key" ON "FungibleAssetTransactionLog"("deckId", "userId");
+CREATE UNIQUE INDEX "FungibleAssetTransactionLog_deckId_userId_key" ON "FungibleAssetTransactionLog"("type", "deckId", "userId");
