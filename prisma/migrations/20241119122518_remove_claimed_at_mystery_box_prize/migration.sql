0adba8d3c3069e729929a5693cef5c535c6d1523
Yash Garg <yash431garg@gmail.com>
2024-11-19 17:56:42 +0530

    remove: claimedAt from schema

---
+++ prisma/migrations/20241119122518_remove_claimed_at_mystery_box_prize/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `claimedAt` on the `MysteryBoxPrize` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "MysteryBoxPrize" DROP COLUMN "claimedAt";
