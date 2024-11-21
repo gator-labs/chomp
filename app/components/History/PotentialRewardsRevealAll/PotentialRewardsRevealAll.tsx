f1e5f42fb9452490d725c23ab0a7af87df066ec6
Danijel Radomiljac <danijel.radomiljac@blankhq.co>
2024-11-21 10:07:18 +0100

    add log

--- app/components/History/PotentialRewardsRevealAll/PotentialRewardsRevealAll.tsx
+++ app/components/History/PotentialRewardsRevealAll/PotentialRewardsRevealAll.tsx
@@ -5,7 +5,7 @@ import { RevealProps } from "@/app/hooks/useReveal";
 import { useRevealedContext } from "@/app/providers/RevealProvider";
 import { numberToCurrencyFormatter } from "@/app/utils/currency";
 import { useQueryClient } from "@tanstack/react-query";
-import { useCallback, useState } from "react";
+import { startTransition, useCallback, useOptimistic, useState } from "react";
 
 import { Button } from "../../Button/Button";
 import { getTotalRevealTokenAmount } from "./helpers";
@@ -26,10 +26,10 @@ export default function PotentialRewardsRevealAll({
   const totalRevealTokenAmount = getTotalRevealTokenAmount(revealableQuestions);
   const maxRewardPerQuestion = totalRevealTokenAmount * 2;
 
-  // const [optimisticMaxRewardPerQuestion, revealOptimistic] = useOptimistic(
-  //   maxRewardPerQuestion,
-  //   (_, optimisticValue: number) => optimisticValue,
-  // );
+  const [optimisticMaxRewardPerQuestion, revealOptimistic] = useOptimistic(
+    maxRewardPerQuestion,
+    (_, optimisticValue: number) => optimisticValue,
+  );
 
   const queryClient = useQueryClient();
   const [isLoading, setIsLoading] = useState(false);
@@ -48,9 +48,9 @@ export default function PotentialRewardsRevealAll({
         ) || []),
       ]);
 
-      // startTransition(() => {
-      //   revealOptimistic(0);
-      // });
+      startTransition(() => {
+        revealOptimistic(0);
+      });
 
       queryClient.resetQueries({
         queryKey: [
@@ -82,10 +82,11 @@ export default function PotentialRewardsRevealAll({
       <div className="flex flex-col justify-between gap-[10px]">
         <div className="text-xs text-white  ">Potential Rewards</div>
         <div className="text-base text-white  font-semibold ">
-          {numberToCurrencyFormatter.format(maxRewardPerQuestion)} BONK
+          {numberToCurrencyFormatter.format(optimisticMaxRewardPerQuestion)}{" "}
+          BONK
         </div>
       </div>
-      {maxRewardPerQuestion !== 0 && (
+      {optimisticMaxRewardPerQuestion !== 0 && (
         <Button
           onClick={handleRevealAll}
           disabled={isLoading}
