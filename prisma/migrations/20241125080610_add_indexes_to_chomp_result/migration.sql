59ee5abbe1b11fabe0f1ef1c8409710342d3cf1c
Danijel Radomiljac <danijel.radomiljac@blankhq.co>
2024-11-25 09:07:32 +0100

    add indexes

---
+++ prisma/migrations/20241125080610_add_indexes_to_chomp_result/migration.sql
@@ -0,0 +1,8 @@
+-- CreateIndex
+CREATE INDEX "ChompResult_burnTransactionSignature_idx" ON "ChompResult"("burnTransactionSignature");
+
+-- CreateIndex
+CREATE INDEX "ChompResult_sendTransactionSignature_idx" ON "ChompResult"("sendTransactionSignature");
+
+-- CreateIndex
+CREATE INDEX "ChompResult_revealNftId_idx" ON "ChompResult"("revealNftId");
