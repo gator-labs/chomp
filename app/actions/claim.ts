c84b13f3d41bacc5a451736e0cd93b3115f0f6d7
Shaun Amott <shaun@inerd.com>
2024-12-09 13:15:31 +0100

    Demote rewardMysteryBox() from being a server action.
    The function should only be called from other server actions so we can
    control the arguments.

--- app/actions/claim.ts
+++ app/actions/claim.ts
@@ -7,11 +7,11 @@ import * as Sentry from "@sentry/nextjs";
 import _ from "lodash";
 import { revalidatePath } from "next/cache";
 
+import { rewardMysteryBox } from "../../lib/mysteryBox";
 import prisma from "../services/prisma";
 import { ONE_MINUTE_IN_MILLISECONDS } from "../utils/dateUtils";
 import { acquireMutex } from "../utils/mutex";
 import { getJwtPayload } from "./jwt";
-import { rewardMysteryBox } from "./mysteryBox/reward";
 
 export async function claimQuestion(questionId: number) {
   console.log("claim questions fired");
@@ -94,10 +94,11 @@ export async function claimAllAvailable() {
   const FF_MYSTERY_BOX = process.env.NEXT_PUBLIC_FF_MYSTERY_BOX_CLAIM_ALL;
 
   const mysteryBoxId = FF_MYSTERY_BOX
-    ? await rewardMysteryBox({
-        triggerType: EBoxTriggerType.ClaimAllCompleted,
-        questionIds: claimableQuestionIds,
-      })
+    ? await rewardMysteryBox(
+        payload.sub,
+        EBoxTriggerType.ClaimAllCompleted,
+        claimableQuestionIds,
+      )
     : null;
 
   const claimResult = await claimQuestions(claimableQuestionIds);
