c8f05ba94de64f18025964458647563aaa58ed31
Yash Garg <yash431garg@gmail.com>
2024-12-17 18:56:04 +0530

    feat: new trigger type schema

--- types/mysteryBox.ts
+++ types/mysteryBox.ts
@@ -2,4 +2,5 @@ export enum MysteryBoxEventsType {
   CLAIM_ALL_COMPLETED = "ClaimAllCompleted",
   DAILY_DECK_COMPLETED = "DailyDeckCompleted",
   CHOMPMAS = "Chompmas",
+  TUTORIAL_COMPLETED = "TutorialCompleted",
 }
