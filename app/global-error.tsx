3bfb96a16998a9f536ecd3075c5cffeaa85ca2eb
Neel <neelp0980@gmail.com>
2024-12-20 17:34:03 +0530

    feat: setup sentry wizard

--- app/global-error.tsx
+++ app/global-error.tsx
@@ -1,5 +1,8 @@
 "use client";
 
+import * as Sentry from "@sentry/nextjs";
+import { useEffect } from "react";
+
 import ErrorBoundary from "./components/ErrorBoundary/ErrorBoundary";
 
 // avoid using useRouter in the global error boundaries as they don't have nextjs context.
@@ -10,6 +13,9 @@ export default function GlobalError({
   error: Error;
   reset: () => void;
 }) {
+  useEffect(() => {
+    Sentry.captureException(error);
+  }, [error]);
   return (
     <html>
       <body className="bg-gray-950 h-[100%]">
