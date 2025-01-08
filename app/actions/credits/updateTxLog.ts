793843f5fc7de1bec5f8d77874095215aa78a213
Yash Garg <yash431garg@gmail.com>
2025-01-08 16:24:04 +0530

    feat: add comments in update tx logs

--- app/actions/credits/updateTxLog.ts
+++ app/actions/credits/updateTxLog.ts
@@ -2,6 +2,19 @@ import prisma from "@/app/services/prisma";
 import { FungibleAsset, TransactionLogType } from "@prisma/client";
 import "server-only";
 
+/**
+ * Add the credit purchase entry in transaction log table linked
+ * to chain tx
+ *
+ * @param hash Chain Tx hash and primary key
+ *
+ * @param deckCost It represent the cost of deck for which user
+ * is trying to purchase credit
+ *
+ * @param userId userId of the authenticated user.
+ *
+ */
+
 export const updateTransactionLog = async (
   hash: string,
   deckCost: number,
