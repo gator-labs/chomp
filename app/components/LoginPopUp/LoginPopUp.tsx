2a754fa52696333a5b3d59285ee40372286e3d25
Marvin Arnold <marvin@geeky.rocks>
2025-01-23 13:04:47 -0600

    build: upgrade to v4 Dynamic
    Follow up needed to full implement authToken correctly.
    
    - change events callback: https://docs.dynamic.xyz/react-sdk/upgrade/v4#eventscallbacks-removed
    - fetch auth token differently: https://docs.dynamic.xyz/react-sdk/upgrade/v4#authtoken-and-minauthtoken

--- app/components/LoginPopUp/LoginPopUp.tsx
+++ app/components/LoginPopUp/LoginPopUp.tsx
@@ -4,6 +4,7 @@ import { setJwt } from "@/app/actions/jwt";
 import LoadingScreen from "@/app/screens/LoginScreens/LoadingScreen";
 import {
   DynamicConnectButton,
+  getAuthToken,
   useDynamicContext,
 } from "@dynamic-labs/sdk-react-core";
 import { RedirectType, redirect } from "next/navigation";
@@ -21,7 +22,9 @@ type LoginPopUpProps = {
 };
 
 const LoginPopUp = ({ isOpen, onClose, userId, deckId }: LoginPopUpProps) => {
-  const { authToken, awaitingSignatureState } = useDynamicContext();
+  const { awaitingSignatureState } = useDynamicContext();
+  const authToken = getAuthToken();
+
   const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
