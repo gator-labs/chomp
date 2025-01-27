49b1266b50795bdd8decb31daa0549c1efdaaacf
Yash Garg <yash431garg@gmail.com>
2025-01-27 21:47:57 +0530

    fix: bottom tab navigation

--- app/components/Navigation/Navigation.tsx
+++ app/components/Navigation/Navigation.tsx
@@ -18,7 +18,7 @@ export function Navigation({ items }: NavigationProps) {
   const pathname = usePathname();
 
   return (
-    <div className="flex justify-center p-1 space-x-6 py-3 w-full bg-gray-800 overflow-x-scroll">
+    <div className="flex justify-center p-1 space-x-6 py-3 w-full bg-gray-800 h-20 overflow-x-auto">
       {items.map((item, index) => (
         <Link
           key={index}
