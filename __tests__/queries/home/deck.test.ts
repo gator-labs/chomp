f0693cb8bcd2de6b3234a15b6e33e8e81c77f504
Neel <neelp0980@gmail.com>
2025-01-27 12:36:15 +0300

    fix: update reveal date deck test

--- __tests__/queries/home/deck.test.ts
+++ __tests__/queries/home/deck.test.ts
@@ -23,133 +23,131 @@ describe("queryExpiringDecks", () => {
   let questionIds: number[] = [];
 
   beforeAll(async () => {
-    await prisma.$transaction(async (tx) => {
-      // Ensure there are no expiring decks already in the system
-      await tx.$queryRaw`
-        UPDATE "Deck" SET "revealAtDate" = "revealAtDate" - INTERVAL '5 years'
+    // Ensure there are no expiring decks already in the system
+    await prisma.$queryRaw`
+      UPDATE "Deck" SET "revealAtDate" = "revealAtDate" - INTERVAL '2 years'
       `;
 
-      // Create decks
-      const decks = await Promise.all([
-        tx.deck.create({
-          data: {
-            deck: "Deck 1",
-            date: new Date(),
-            revealAtDate: dayjs().add(1, "day").toDate(),
-          },
-        }),
-        tx.deck.create({
-          data: {
-            deck: "Deck 2",
-            date: new Date(),
-            revealAtDate: dayjs().add(1, "day").toDate(),
-          },
-        }),
-      ]);
+    // Create decks
+    const decks = await Promise.all([
+      prisma.deck.create({
+        data: {
+          deck: "Deck 1",
+          date: new Date(),
+          revealAtDate: dayjs().add(1, "day").toDate(),
+        },
+      }),
+      prisma.deck.create({
+        data: {
+          deck: "Deck 2",
+          date: new Date(),
+          revealAtDate: dayjs().add(1, "day").toDate(),
+        },
+      }),
+    ]);
 
-      deckIds = decks.map((deck) => deck.id);
+    deckIds = decks.map((deck) => deck.id);
 
-      // Create questions for decks
-      const questions = await Promise.all([
-        tx.question.create({
-          data: {
-            question: "Is the sky blue?",
-            type: QuestionType.BinaryQuestion,
-            revealAtDate: new Date("2024-10-11 16:00:00.000"),
-            revealToken: Token.Bonk,
-            revealTokenAmount: 5000,
-            questionOptions: {
-              createMany: {
-                data: [
-                  {
-                    option: "Yes",
-                    isLeft: true,
-                    calculatedIsCorrect: true,
-                    calculatedPercentageOfSelectedAnswers: 90,
-                    calculatedAveragePercentage: 70,
-                  },
-                  {
-                    option: "No",
-                    isLeft: false,
-                    calculatedIsCorrect: false,
-                    calculatedPercentageOfSelectedAnswers: 10,
-                    calculatedAveragePercentage: 30,
-                  },
-                ],
-              },
+    // Create questions for decks
+    const questions = await Promise.all([
+      prisma.question.create({
+        data: {
+          question: "Is the sky blue?",
+          type: QuestionType.BinaryQuestion,
+          revealAtDate: dayjs().add(1, "day").toDate(),
+          revealToken: Token.Bonk,
+          revealTokenAmount: 5000,
+          questionOptions: {
+            createMany: {
+              data: [
+                {
+                  option: "Yes",
+                  isLeft: true,
+                  calculatedIsCorrect: true,
+                  calculatedPercentageOfSelectedAnswers: 90,
+                  calculatedAveragePercentage: 70,
+                },
+                {
+                  option: "No",
+                  isLeft: false,
+                  calculatedIsCorrect: false,
+                  calculatedPercentageOfSelectedAnswers: 10,
+                  calculatedAveragePercentage: 30,
+                },
+              ],
             },
           },
-          include: {
-            questionOptions: true,
-          },
-        }),
-        tx.question.create({
-          data: {
-            question: "Is water wet?",
-            type: QuestionType.BinaryQuestion,
-            revealAtDate: new Date("2024-10-12 16:00:00.000"),
-            revealToken: Token.Bonk,
-            revealTokenAmount: 5000,
-            questionOptions: {
-              createMany: {
-                data: [
-                  {
-                    option: "Yes",
-                    isLeft: true,
-                    calculatedIsCorrect: true,
-                    calculatedPercentageOfSelectedAnswers: 85,
-                    calculatedAveragePercentage: 60,
-                  },
-                  {
-                    option: "No",
-                    isLeft: false,
-                    calculatedIsCorrect: false,
-                    calculatedPercentageOfSelectedAnswers: 15,
-                    calculatedAveragePercentage: 40,
-                  },
-                ],
-              },
+        },
+        include: {
+          questionOptions: true,
+        },
+      }),
+      prisma.question.create({
+        data: {
+          question: "Is water wet?",
+          type: QuestionType.BinaryQuestion,
+          revealAtDate: dayjs().add(1, "day").toDate(),
+          revealToken: Token.Bonk,
+          revealTokenAmount: 5000,
+          questionOptions: {
+            createMany: {
+              data: [
+                {
+                  option: "Yes",
+                  isLeft: true,
+                  calculatedIsCorrect: true,
+                  calculatedPercentageOfSelectedAnswers: 85,
+                  calculatedAveragePercentage: 60,
+                },
+                {
+                  option: "No",
+                  isLeft: false,
+                  calculatedIsCorrect: false,
+                  calculatedPercentageOfSelectedAnswers: 15,
+                  calculatedAveragePercentage: 40,
+                },
+              ],
             },
           },
-          include: {
-            questionOptions: true,
-          },
-        }),
-      ]);
+        },
+        include: {
+          questionOptions: true,
+        },
+      }),
+    ]);
 
-      questionIds = questions.map((q) => q.id);
+    questionIds = questions.map((q) => q.id);
 
-      await tx.deckQuestion.createMany({
-        data: [
-          { deckId: decks[0].id, questionId: questions[0].id },
-          { deckId: decks[1].id, questionId: questions[1].id },
-        ],
-      });
+    await prisma.deckQuestion.createMany({
+      data: [
+        { deckId: decks[0].id, questionId: questions[0].id },
+        { deckId: decks[1].id, questionId: questions[1].id },
+      ],
+    });
 
-      // Create users
-      await Promise.all([
-        tx.user.create({ data: user1 }),
-        tx.user.create({ data: user2 }),
-      ]);
+    // Create users
+    await Promise.all([
+      prisma.user.create({ data: user1 }),
+      prisma.user.create({ data: user2 }),
+    ]);
 
-      // Create answers for user1
-      await tx.questionAnswer.createMany({
-        data: questions.flatMap((question) =>
-          question.questionOptions.map((qo, i) => ({
-            questionOptionId: qo.id,
-            userId: user1.id,
-            selected: i === 0,
-          })),
-        ),
-      });
-
-      await tx.questionAnswer.createMany({
-        data: questions[0].questionOptions.map((qo, i) => ({
+    // Create answers for user1
+    await prisma.questionAnswer.createMany({
+      data: questions.flatMap((question) =>
+        question.questionOptions.map((qo, i) => ({
           questionOptionId: qo.id,
-          userId: user2.id,
+          userId: user1.id,
           selected: i === 0,
         })),
-      });
+      ),
+    });
+
+    await prisma.questionAnswer.createMany({
+      data: questions[0].questionOptions.map((qo, i) => ({
+        questionOptionId: qo.id,
+        userId: user2.id,
+        selected: i === 0,
+      })),
     });
   });
 
@@ -175,7 +173,7 @@ describe("queryExpiringDecks", () => {
 
       // Reset dates that were changed for the test
       await tx.$queryRaw`
-        UPDATE "Deck" SET "revealAtDate" = "revealAtDate" + INTERVAL '5 years'
+        UPDATE "Deck" SET "revealAtDate" = "revealAtDate" + INTERVAL '2 years'
       `;
     });
   });
@@ -183,16 +181,12 @@ describe("queryExpiringDecks", () => {
   it("should return decks expiring today with unanswered questions for user2", async () => {
     const result = await queryExpiringDecks(user2.id);
 
-    console.log(result);
-
     expect(result.length).toBe(1); // Only Deck 2 has unanswered questions for user2
     expect(result[0].deck).toBe("Deck 2");
   });
 
   it("should return an empty array for user1 as all questions are answered", async () => {
     const result = await queryExpiringDecks(user1.id);
-
-    console.log(result);
 
     expect(result.length).toBe(0); // user1 has answered all the questions in both decks
   });
