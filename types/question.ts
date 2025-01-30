bf3d4d80dacc6b1aa1e0c3bf000fdfca52fe64c9
Shaun Amott <shaun@inerd.com>
2025-01-30 01:45:05 +0100

    Add QuestionCardStatus component.

--- types/question.ts
+++ types/question.ts
@@ -2,3 +2,9 @@ export enum QuestionStep {
   AnswerQuestion = 1,
   PickPercentage = 2,
 }
+
+export type QuestionCardIndicatorType =
+  | "correct"
+  | "incorrect"
+  | "unanswered"
+  | "unrevealed";
