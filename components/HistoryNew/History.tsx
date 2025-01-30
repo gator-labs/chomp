5a72405d7a0748771b9eea47b80fc98d06c80757
Shaun Amott <shaun@inerd.com>
2025-01-30 18:32:01 +0100

    Add new history component.

---
+++ components/HistoryNew/History.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import useInfiniteQuestionsHistory from "@/app/_reactQuery/useInfiniteQuestionsHistory";
+import HistoryListSkeleton from "@/app/components/HistoryListSkeleton/HistoryListSkeleton";
+import Skeleton from "@/app/components/Skeleton/Skeleton";
+
+import { QuestionCard } from "./QuestionCard";
+
+interface HistoryProps {
+  deckId?: number;
+  deckTitle: string;
+}
+
+export default function History({ deckId, deckTitle }: HistoryProps) {
+  const { data, isFetchingNextPage, lastElementRef, isLoading } =
+    useInfiniteQuestionsHistory(deckId);
+
+  if (isLoading) return <HistoryListSkeleton />;
+
+  return (
+    <div className="flex flex-col gap-2 overflow-hidden">
+      <ul className="flex flex-col gap-2 overflow-y-auto pb-2">
+        {data?.map((question) => (
+          <QuestionCard
+            key={question.id}
+            title={question.question}
+            deckTitle={deckTitle}
+            questionId={question.id}
+            indicatorType={"correct"}
+            ref={lastElementRef}
+          />
+        ))}
+        {isFetchingNextPage && <Skeleton />}
+      </ul>
+    </div>
+  );
+}
