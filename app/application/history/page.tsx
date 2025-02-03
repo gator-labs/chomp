6e0f1bb29ee4714690ccfe76c0957c4f40d94b8f
Shaun Amott <shaun@inerd.com>
2025-02-03 12:10:13 +0100

    Remove box reopening feature.

--- app/application/history/page.tsx
+++ app/application/history/page.tsx
@@ -3,10 +3,7 @@ import History from "@/app/components/History/History";
 import HistoryHeader from "@/app/components/HistoryHeader/HistoryHeader";
 import ProfileNavigation from "@/app/components/ProfileNavigation/ProfileNavigation";
 import { getAllQuestionsReadyForReveal } from "@/app/queries/history";
-import { getUnopenedMysteryBox } from "@/app/queries/mysteryBox";
 import { getProfileImage } from "@/app/queries/profile";
-import { ReopenMysteryBox } from "@/components/MysteryBox/ReopenMysteryBox";
-import { EBoxTriggerType } from "@prisma/client";
 
 export default async function Page() {
   const [revealableQuestions, totalClaimableRewards, profileImg, mysteryBoxId] =
