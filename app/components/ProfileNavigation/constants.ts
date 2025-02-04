f200b449ae4f00a2469df9a451fe89304cb4d6e4
Shaun Amott <shaun@inerd.com>
2025-02-04 10:19:39 +0100

    Rename Rewards tab to History.

--- app/components/ProfileNavigation/constants.ts
+++ app/components/ProfileNavigation/constants.ts
@@ -3,5 +3,5 @@ import { HISTORY_PATH, HOME_PATH, LEADERBOARD_PATH } from "@/lib/urls";
 export const PROFILE_LINKS = [
   { label: "Dashboard", href: HOME_PATH },
   { label: "Leaderboard", href: LEADERBOARD_PATH },
-  { label: "Reveal", href: HISTORY_PATH },
+  { label: "History", href: HISTORY_PATH },
 ];
