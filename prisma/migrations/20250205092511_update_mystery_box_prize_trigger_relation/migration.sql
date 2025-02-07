6bcb9db61051495f8c4eb7fadf2730c7b181fdd3
Yash Garg <yash431garg@gmail.com>
2025-02-07 12:57:05 +0530

    feat: add migration and query to update data for new user mb and chompmas streak b

--- prisma/migrations/20250205092511_update_mystery_box_prize_trigger_relation/migration.sql
+++ prisma/migrations/20250205092511_update_mystery_box_prize_trigger_relation/migration.sql
@@ -1,15 +1,21 @@
-/*
-  Warnings:
+-- Step 1: Add the new column
+ALTER TABLE "MysteryBoxPrize" ADD COLUMN "mysteryBoxTriggerId" TEXT;
 
-  - You are about to drop the column `mysteryBoxId` on the `MysteryBoxPrize` table. All the data in the column will be lost.
+-- Step 2: Populate the new column with the appropriate values
+UPDATE "MysteryBoxPrize"
+SET "mysteryBoxTriggerId" = subquery."id"
+FROM (
+    SELECT "MysteryBoxPrize"."id" AS "prizeId", "MysteryBoxTrigger"."id" AS "id"
+    FROM "MysteryBoxTrigger"
+    JOIN "MysteryBox" ON "MysteryBox".id = "MysteryBoxTrigger"."mysteryBoxId"
+    JOIN "MysteryBoxPrize" ON "MysteryBox".id = "MysteryBoxPrize"."mysteryBoxId"
+    WHERE "MysteryBoxTrigger"."triggerType" IN ('TutorialCompleted', 'ChompmasStreakAttained')
+) AS subquery
+WHERE "MysteryBoxPrize"."id" = subquery."prizeId";
 
-*/
--- DropForeignKey
-ALTER TABLE "MysteryBoxPrize" DROP CONSTRAINT "MysteryBoxPrize_mysteryBoxId_fkey";
-
--- AlterTable
-ALTER TABLE "MysteryBoxPrize" DROP COLUMN "mysteryBoxId",
-ADD COLUMN     "mysteryBoxTriggerId" TEXT;
-
--- AddForeignKey
+-- Step 3: Add the foreign key constraint for the new column
 ALTER TABLE "MysteryBoxPrize" ADD CONSTRAINT "MysteryBoxPrize_mysteryBoxTriggerId_fkey" FOREIGN KEY ("mysteryBoxTriggerId") REFERENCES "MysteryBoxTrigger"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- Step 4: Drop the old foreign key constraint and the old column
+ALTER TABLE "MysteryBoxPrize" DROP CONSTRAINT "MysteryBoxPrize_mysteryBoxId_fkey";
+ALTER TABLE "MysteryBoxPrize" DROP COLUMN "mysteryBoxId";
