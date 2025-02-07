5daa214b8e4565dad1b6e0c89b117bf8b4f3a1ce
Yash Garg <yash431garg@gmail.com>
2025-02-07 16:24:56 +0530

    fix query

--- prisma/migrations/20250205092511_update_mystery_box_prize_trigger_relation/migration.sql
+++ prisma/migrations/20250205092511_update_mystery_box_prize_trigger_relation/migration.sql
@@ -2,7 +2,7 @@
 ALTER TYPE "EBoxTriggerType" ADD VALUE 'CombinedTriggers';
 
 -- Step 2: Add the new column
-ALTER TABLE "MysteryBoxPrize" ADD COLUMN "mysteryBoxTriggerId" UUID;
+ALTER TABLE "MysteryBoxPrize" ADD COLUMN "mysteryBoxTriggerId" TEXT;
 
 -- Step 3: Populate the new column with the appropriate values for the initial query
 UPDATE "MysteryBoxPrize"
@@ -18,27 +18,27 @@ WHERE "MysteryBoxPrize"."id" = subquery."prizeId";
 
 -- Step 4: Select the triggers for 'ClaimAllCompleted' and 'RevealAllCompleted'
 WITH new_triggers AS (
-SELECT DISTINCT ON ("MysteryBoxTrigger"."mysteryBoxId") 
-    "MysteryBoxTrigger"."id" AS "triggerId", 
-    "MysteryBoxTrigger"."mysteryBoxId" AS "mysteryBoxId"
-FROM "MysteryBoxTrigger"
-JOIN "MysteryBox" ON "MysteryBox".id = "MysteryBoxTrigger"."mysteryBoxId"
-WHERE "MysteryBoxTrigger"."triggerType" IN ('ClaimAllCompleted', 'RevealAllCompleted')
-ORDER BY "MysteryBoxTrigger"."mysteryBoxId", "MysteryBoxTrigger"."id";
-)
-
+    SELECT DISTINCT ON (mbt."mysteryBoxId") 
+        mbt."id" AS "triggerId", 
+        mbt."mysteryBoxId" AS "mysteryBoxId"
+    FROM "MysteryBoxTrigger" mbt
+    JOIN "MysteryBox" mb ON mb."id" = mbt."mysteryBoxId"
+    WHERE mbt."triggerType" IN ('ClaimAllCompleted', 'RevealAllCompleted')
+    ORDER BY mbt."mysteryBoxId", mbt."id"
+),
 -- Insert new triggers with 'CombinedTriggers' for each distinct mysteryBoxId
 inserted_triggers AS (
     INSERT INTO "MysteryBoxTrigger" ("id", "mysteryBoxId", "triggerType")
-    SELECT gen_random_uuid(), "mysteryBoxId", 'CombinedTriggers'
-    FROM new_triggers
+    SELECT gen_random_uuid(), nt."mysteryBoxId", 'CombinedTriggers'
+    FROM new_triggers nt
     RETURNING "id", "mysteryBoxId"
 )
 -- Update the new column in MysteryBoxPrize with the new trigger id
-UPDATE "MysteryBoxPrize"
-SET "mysteryBoxTriggerId" = inserted_triggers."id"
-FROM inserted_triggers
-WHERE "MysteryBoxPrize"."mysteryBoxId" = inserted_triggers."mysteryBoxId";
+UPDATE "MysteryBoxPrize" mbp
+SET "mysteryBoxTriggerId" = it."id"
+FROM inserted_triggers it
+WHERE mbp."mysteryBoxId" = it."mysteryBoxId";
+
 
 -- Step 5: Add the foreign key constraint for the new column
 ALTER TABLE "MysteryBoxPrize" ADD CONSTRAINT "MysteryBoxPrize_mysteryBoxTriggerId_fkey" FOREIGN KEY ("mysteryBoxTriggerId") REFERENCES "MysteryBoxTrigger"("id") ON DELETE SET NULL ON UPDATE CASCADE;
