1a3bf75eda7f0ee23f777c2fbbb3edfa7067304f
Shaun Amott <shaun@inerd.com>
2025-02-08 20:51:56 +0100

    Automatically flag suspected bots.

---
+++ app/api/cron/detect-bot-activity/route.ts
@@ -0,0 +1,69 @@
+import { getQuestionsNeedingCorrectAnswer } from "@/app/queries/answers";
+import { calculateCorrectAnswer } from "@/app/utils/algo";
+import { tryAcquireMutex } from "@/app/utils/mutex";
+import { isBotUpdateNeeded, updateBots } from "@/lib/bots";
+
+const API_TIMEOUT = 5 * 60 * 1000; // Five minutes
+
+/**
+ * Looks for questions that are revealing soon and submits
+ * them to the bot analysis API, marking any bots that are
+ * returned from the API as bots in the database.
+ */
+export async function GET(request: Request) {
+  const secret = process.env.CRON_SECRET || "";
+
+  const authHeader = request.headers.get("authorization");
+  if (authHeader !== `Bearer ${secret}`) {
+    return new Response("Unauthorized", { status: 401 });
+  }
+
+  const release = await tryAcquireMutex({
+    identifier: "DETECT_BOT_ACTIVITY",
+    data: {},
+  });
+
+  if (release === null) {
+    return new Response(
+      JSON.stringify({
+        message: "Activity analysis already in progress",
+      }),
+      {
+        status: 200,
+        headers: { "Content-Type": "application/json" },
+      },
+    );
+  }
+
+  try {
+    const results = await response.json();
+    const response = fetch(
+      "https://mechanism-engine.vercel.app/api/chomp/bot-detector",
+      { signal: AbortSignal.timeout(FIVE_MINUTES) },
+    );
+
+    if (!("mean_bot_score" in results) || !results["mean_bot_score"])
+      throw new Error("Missing field in bot detector API results");
+
+    const scores: Record<string, number> = results["mean_bot_score"];
+    const botUserIds = Object.keys(scores);
+
+    await updateBots(botUserIds);
+
+    return new Response(
+      JSON.stringify({
+        message: "Analysis completed",
+        botCount: botUserIds.length,
+      }),
+      {
+        status: 200,
+        headers: { "Content-Type": "application/json" },
+      },
+    );
+  } catch (error) {
+    console.error("Error:", error);
+    return new Response("Internal Server Error", { status: 500 });
+  } finally {
+    release();
+  }
+}
--- lib/bots.ts
+++ lib/bots.ts
@@ -1,5 +1,11 @@
 import prisma from "@/app/services/prisma";
 
+/**
+ * Marks the given list of suspected bot users as
+ * bots, without overwriting existing threat levels.
+ *
+ * @param bots Array of user IDs.
+ */
 export const updateBots = async (bots: string[]) => {
   await prisma.user.updateMany({
     data: {
