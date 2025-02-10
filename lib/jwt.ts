13cd5b8752488d674ae6043905d62f6fb29affc2
Shaun Amott <shaun@inerd.com>
2025-02-11 00:17:07 +0100

    Check for ManualBlock scenario.

--- lib/jwt.ts
+++ lib/jwt.ts
@@ -1,11 +1,15 @@
 import prisma from "@/app/services/prisma";
 import { UserThreatLevelDetected } from "@/lib/error";
+import { EThreatLevelType } from "@/types/bots";
 import { cookies } from "next/headers";
 
 export const checkThreatLevel = async (userId: string) => {
   const user = await prisma.user.findFirst({ where: { id: userId } });
   if (!user) throw new Error("User not found");
-  if (user.threatLevel === "bot") {
+  if (
+    user.threatLevel === EThreatLevelType.Bot ||
+    user.threatLevel === EThreatLevelType.ManualBlock
+  ) {
     throw new UserThreatLevelDetected("User threat level detected");
   }
 };
