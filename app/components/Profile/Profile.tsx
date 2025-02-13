016b73fad8c87ea2e5598939e9b338c357d65dfc
Shaun Amott <shaun@inerd.com>
2025-02-13 22:05:33 +0100

    Make buy credits button on profile small.

--- app/components/Profile/Profile.tsx
+++ app/components/Profile/Profile.tsx
@@ -64,7 +64,7 @@ export async function Profile({
               )}
             </Button>
 
-            <BuyBulkCreditsButton />
+            <BuyBulkCreditsButton size="small" />
           </div>
           {showLeaderboardButton && (
             <Link href="/application/leaderboard" className="contents">
