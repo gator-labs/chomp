7e203ae9e1f08fe032d788bfc876831a50bf06df
Yash Garg <yash431garg@gmail.com>
2025-02-19 17:45:06 +0530

    update global.d.ts

--- global.d.ts
+++ global.d.ts
@@ -1,5 +1,10 @@
 declare module "*.svg" {
-  import React from "react";
-  const SVG: React.VFC<React.SVGProps<SVGSVGElement>>;
-  export default SVG;
+  import * as React from "react";
+
+  export const ReactComponent: React.FunctionComponent<
+    React.SVGProps<SVGSVGElement> & { title?: string }
+  >;
+
+  const src: string;
+  export default src;
 }
