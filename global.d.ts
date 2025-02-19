be3821ad2af5a421d67701147a8fdcfb155a500f
Yash Garg <yash431garg@gmail.com>
2025-02-19 17:50:10 +0530

    change svg type module to next/image

--- global.d.ts
+++ global.d.ts
@@ -1,10 +1,5 @@
 declare module "*.svg" {
-  import * as React from "react";
-
-  export const ReactComponent: React.FunctionComponent<
-    React.SVGProps<SVGSVGElement> & { title?: string }
-  >;
-
-  const src: string;
-  export default src;
+  import { StaticImageData } from "next/image";
+  const content: StaticImageData;
+  export default content;
 }
