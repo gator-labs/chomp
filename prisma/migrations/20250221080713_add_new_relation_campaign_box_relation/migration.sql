99e13dd81793fd060c0c5400c68b236852b3adc2
Yash Garg <yash431garg@gmail.com>
2025-02-21 13:38:03 +0530

    refactor: fix typo in campaign

--- prisma/migrations/20250221080713_add_new_relation_campaign_box_relation/migration.sql
+++ prisma/migrations/20250221080713_add_new_relation_campaign_box_relation/migration.sql
@@ -3,17 +3,17 @@ CREATE TABLE "CampaignMysteryBoxAllowed" (
     "id" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "campignId" TEXT NOT NULL,
+    "campaignId" TEXT NOT NULL,
     "allowlistAddress" TEXT NOT NULL,
 
     CONSTRAINT "CampaignMysteryBoxAllowed_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "CampaignMysteryBoxAllowed_campignId_allowlistAddress_key" ON "CampaignMysteryBoxAllowed"("campignId", "allowlistAddress");
+CREATE UNIQUE INDEX "CampaignMysteryBoxAllowed_campaignId_allowlistAddress_key" ON "CampaignMysteryBoxAllowed"("campaignId", "allowlistAddress");
 
 -- AddForeignKey
-ALTER TABLE "CampaignMysteryBoxAllowed" ADD CONSTRAINT "CampaignMysteryBoxAllowed_campignId_fkey" FOREIGN KEY ("campignId") REFERENCES "CampaignMysteryBox"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "CampaignMysteryBoxAllowed" ADD CONSTRAINT "CampaignMysteryBoxAllowed_campaignId_fkey" FOREIGN KEY ("campaignId") REFERENCES "CampaignMysteryBox"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "CampaignMysteryBoxAllowed" ADD CONSTRAINT "CampaignMysteryBoxAllowed_allowlistAddress_fkey" FOREIGN KEY ("allowlistAddress") REFERENCES "MysteryBoxAllowlist"("address") ON DELETE RESTRICT ON UPDATE CASCADE;
