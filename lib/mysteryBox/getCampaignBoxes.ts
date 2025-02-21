748766ce47d5b2eb21dc0cec2a3111588181e282
Yash Garg <yash431garg@gmail.com>
2025-02-21 15:42:10 +0530

    refactor: change getCampaignBox flow

---
+++ lib/mysteryBox/getCampaignBoxes.ts
@@ -0,0 +1,41 @@
+import { getJwtPayload } from "@/app/actions/jwt";
+import prisma from "@/app/services/prisma";
+import "server-only";
+
+export const getCampaignBoxes = async () => {
+  const payload = await getJwtPayload();
+
+  if (!payload) {
+    return null;
+  }
+
+  const userWallet = await prisma.wallet.findFirst({
+    where: { userId: payload.sub },
+    select: {
+      address: true,
+    },
+  });
+
+  if (!userWallet) return null;
+
+  const campaignBoxes = await prisma.campaignMysteryBox.findMany();
+
+  const eligibleBoxes = await prisma.campaignMysteryBoxAllowed.findMany({
+    where: {
+      allowlistAddress: userWallet.address,
+    },
+    select: {
+      id: true,
+      campaignId: true,
+    },
+  });
+
+  const campaignIds = new Set(eligibleBoxes.map((eb) => eb.campaignId));
+
+  const updatedData = campaignBoxes.map((cb) => ({
+    ...cb,
+    isEligible: campaignIds.has(cb.id),
+  }));
+
+  return updatedData;
+};
