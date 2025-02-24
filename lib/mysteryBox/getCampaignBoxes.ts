5ab3cde4269d757bf9b6e18bf2d140533e1b6742
Yash Garg <yash431garg@gmail.com>
2025-02-24 16:35:30 +0530

    feat: add constraint for existing campaign box

--- lib/mysteryBox/getCampaignBoxes.ts
+++ lib/mysteryBox/getCampaignBoxes.ts
@@ -1,5 +1,6 @@
 import { getJwtPayload } from "@/app/actions/jwt";
 import prisma from "@/app/services/prisma";
+import { EMysteryBoxStatus } from "@prisma/client";
 import "server-only";
 
 export const getCampaignBoxes = async () => {
@@ -20,7 +21,7 @@ export const getCampaignBoxes = async () => {
 
   const campaignBoxes = await prisma.campaignMysteryBox.findMany();
 
-  const eligibleBoxes = await prisma.campaignMysteryBoxAllowed.findMany({
+  const validCampaignBoxes = await prisma.campaignMysteryBoxAllowed.findMany({
     where: {
       allowlistAddress: userWallet.address,
     },
@@ -30,13 +31,33 @@ export const getCampaignBoxes = async () => {
     },
   });
 
-  const campaignIds = new Set(
-    eligibleBoxes.map((eb) => eb.campaignMysteryBoxId),
+  const openedMysteryBoxes = await prisma.mysteryBoxTrigger.findMany({
+    where: {
+      campaignMysteryBoxId: {
+        in: validCampaignBoxes.map((eb) => eb.campaignMysteryBoxId),
+      },
+    },
+    include: {
+      MysteryBox: {
+        where: {
+          status: { in: [EMysteryBoxStatus.Opened] },
+        },
+      },
+    },
+  });
+
+  const claimedCampaignIds = openedMysteryBoxes.map((epb) =>
+    epb.MysteryBox ? epb.campaignMysteryBoxId : null,
+  );
+
+  const campaignBoxIds = new Set(
+    validCampaignBoxes.map((eb) => eb.campaignMysteryBoxId),
   );
 
   const updatedData = campaignBoxes.map((cb) => ({
     ...cb,
-    isEligible: campaignIds.has(cb.id),
+    isEligible:
+      campaignBoxIds.has(cb.id) && !claimedCampaignIds.includes(cb.id),
   }));
 
   return updatedData;
