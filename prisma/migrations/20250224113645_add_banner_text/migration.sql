356ea42ebbf962d9250ce3ffd7eb665898cca8cd
Shaun Amott <shaun@inerd.com>
2025-02-25 01:00:40 +0100

    Clear out legacy banners before migration is applied.

--- prisma/migrations/20250224113645_add_banner_text/migration.sql
+++ prisma/migrations/20250224113645_add_banner_text/migration.sql
@@ -4,5 +4,9 @@
   - Added the required column `text` to the `Banner` table without a default value. This is not possible if the table is not empty.
 
 */
+
+-- Clean up legacy banners
+DELETE FROM "Banner";
+
 -- AlterTable
 ALTER TABLE "Banner" ADD COLUMN     "text" TEXT NOT NULL;
