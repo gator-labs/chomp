775d3fb90f1a59bda0016b9d66ff34c8106910c1
Shaun Amott <shaun@inerd.com>
2025-03-07 22:07:31 +0100

    Move bot detector endpoint query to its own function.

--- app/api/cron/detect-bot-activity/route.ts
+++ app/api/cron/detect-bot-activity/route.ts
@@ -1,9 +1,8 @@
 import { SENTRY_FLUSH_WAIT } from "@/app/constants/sentry";
 import { tryAcquireMutex } from "@/app/utils/mutex";
 import { updateBots } from "@/lib/bots";
+import { queryBotDetector } from '@/lib/bot-detector';
 import * as Sentry from "@sentry/nextjs";
-
-const API_TIMEOUT = 5 * 60 * 1000; // Five minutes
 
 /**
  * Looks for questions that are revealing soon and submits
@@ -36,25 +35,9 @@ export async function GET(request: Request) {
   }
 
   try {
-    if (!process.env.MECHANISM_ENGINE_URL)
-      throw new Error("MECHANISM_ENGINE_URL not defined");
+    const results = await queryBotDetector();
 
-    const response = await fetch(
-      process.env.MECHANISM_ENGINE_URL + "/api/chomp/bot-detector",
-      { signal: AbortSignal.timeout(API_TIMEOUT) },
-    );
-
-    if (!response.ok)
-      throw new Error(
-        `Mechanism engine bot detector returned error: ${response.status}: ${response.statusText}`,
-      );
-
-    const results = await response.json();
-
-    if (!("mean_bot_score" in results) || !results["mean_bot_score"])
-      throw new Error("Missing field in bot detector API results");
-
-    const scores: Record<string, number> = results["mean_bot_score"];
+    const scores = results["mean_bot_score"];
     const botUserIds = Object.keys(scores);
 
     await updateBots(
---
+++ lib/bot-detector.ts
@@ -0,0 +1,29 @@
+const API_TIMEOUT = 5 * 60 * 1000; // Five minutes
+
+export type BotDetectorResponse = {
+  start_date: string;
+  end_date: string;
+  mean_bot_score: Record<string, number>;
+};
+
+export const queryBotDetector = async(): Promise<BotDetectorResponse> => {
+  if (!process.env.MECHANISM_ENGINE_URL)
+    throw new Error("MECHANISM_ENGINE_URL not defined");
+
+  const response = await fetch(
+    process.env.MECHANISM_ENGINE_URL + "/api/chomp/bot-detector",
+    { signal: AbortSignal.timeout(API_TIMEOUT) },
+  );
+
+  if (!response.ok)
+    throw new Error(
+      `Mechanism engine bot detector returned error: ${response.status}: ${response.statusText}`,
+    );
+
+    const results = await response.json();
+
+    if (!("mean_bot_score" in results) || !results["mean_bot_score"])
+      throw new Error("Missing field in bot detector API results");
+
+    return results;
+}
