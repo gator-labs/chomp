d9b6cde45a10c3513c5062bf2c00435d60fc4bbb
Shaun Amott <shaun@inerd.com>
2025-03-20 14:32:06 +0100

    Add engineering block/allow threat levels.

--- types/bots.ts
+++ types/bots.ts
@@ -8,8 +8,18 @@ export enum EThreatLevelType {
   // by the bot checker if they misbehave.
   ManualAllow = "manual-allow",
 
+  // As above, but block is for engineering
+  // reasons
+  EngAllow = "eng-allow",
+
   // User who has been blocked manually
   ManualBlock = "manual-block",
+
+  // As above, but block is for engineering
+  // reasons; a different error will be shown
+  // to users so that complaints are easier to
+  // triage.
+  EngBlock = "eng-block",
 
   // User who is allowed to access the app
   // and won't be blocked by the bot checker.
