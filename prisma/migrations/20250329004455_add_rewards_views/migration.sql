2c276a768b7c30f31f093d591233943745fe1236
Shaun Amott <shaun@inerd.com>
2025-03-29 00:55:07 +0100

    Trailing whitespace.

--- prisma/migrations/20250329004455_add_rewards_views/migration.sql
+++ prisma/migrations/20250329004455_add_rewards_views/migration.sql
@@ -7,7 +7,7 @@ SELECT
     FROM "DeckQuestion" dq
     LEFT JOIN "MysteryBoxTrigger" mbt ON mbt."questionId" = dq."questionId"
     LEFT JOIN "MysteryBox" mb ON mbt."mysteryBoxId" = mb.id
-    LEFT JOIN "MysteryBoxPrize" mbp ON mbp."mysteryBoxTriggerId" = mbt.id 
+    LEFT JOIN "MysteryBoxPrize" mbp ON mbp."mysteryBoxTriggerId" = mbt.id
     GROUP BY ("userId", dq."deckId");
 
 CREATE VIEW "QuestionRewards" AS
@@ -16,7 +16,7 @@ SELECT
     "questionId",
     SUM(CASE WHEN "prizeType" = 'Token' AND "tokenAddress" = 'DezXAZ8z7PnrnRJjz3wXBoRgixCa6xjnB7YaB1pPB263' THEN CAST("amount" AS NUMERIC) ELSE NULL END) AS "bonkReward",
     SUM(CASE WHEN "prizeType" = 'Credits' THEN CAST("amount" AS NUMERIC) ELSE NULL END) AS "creditsReward"
-    FROM "MysteryBoxTrigger" mbt 
+    FROM "MysteryBoxTrigger" mbt
     LEFT JOIN "MysteryBox" mb ON mbt."mysteryBoxId" = mb.id
-    LEFT JOIN "MysteryBoxPrize" mbp ON mbp."mysteryBoxTriggerId" = mbt.id 
+    LEFT JOIN "MysteryBoxPrize" mbp ON mbp."mysteryBoxTriggerId" = mbt.id
     GROUP BY("userId", "questionId");
