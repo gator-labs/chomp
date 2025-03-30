b2477562143f6bb9f57c95fb37b17c872e1d56c1
Shaun Amott <shaun@inerd.com>
2025-03-31 00:00:41 +0200

    Grammar.

--- app/admin/ask-inbox/page.tsx
+++ app/admin/ask-inbox/page.tsx
@@ -11,7 +11,7 @@ export default function Page() {
   if (askList.isLoading || !askList.data) return <div>Loading...</div>;
 
   if (askList.data.askList.length == 0)
-    return <div>No unassigned questions found</div>;
+    return <div>No unassigned questions found.</div>;
 
   return (
     <div className="flex flex-col gap-2">
