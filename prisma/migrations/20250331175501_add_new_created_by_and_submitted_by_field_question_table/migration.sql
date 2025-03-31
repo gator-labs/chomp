6ced11a81faca2e0d1b730939e9419a56c3bd997
Shaun Amott <shaun@inerd.com>
2025-03-31 23:17:54 +0200

    Schema: createdBy -> createdByUserId.

--- prisma/migrations/20250331175501_add_new_created_by_and_submitted_by_field_question_table/migration.sql
+++ prisma/migrations/20250331175501_add_new_created_by_and_submitted_by_field_question_table/migration.sql
@@ -1,6 +1,6 @@
 -- AlterTable
-ALTER TABLE "Question" ADD COLUMN     "createdBy" TEXT,
+ALTER TABLE "Question" ADD COLUMN     "createdByUserId" TEXT,
 ADD COLUMN     "isSubmittedByUser" BOOLEAN NOT NULL DEFAULT false;
 
 -- AddForeignKey
-ALTER TABLE "Question" ADD CONSTRAINT "Question_createdBy_fkey" FOREIGN KEY ("createdBy") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE "Question" ADD CONSTRAINT "Question_createdByUserId_fkey" FOREIGN KEY ("createdByUserId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
