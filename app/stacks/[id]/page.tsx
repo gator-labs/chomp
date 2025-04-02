918513d583ddb7fc82950e5b15ac3a471f08e703
Shaun Amott <shaun@inerd.com>
2025-04-02 12:50:25 +0200

    Don't die if no stack image; check FF before viewing community stack.

--- app/stacks/[id]/page.tsx
+++ app/stacks/[id]/page.tsx
@@ -3,6 +3,7 @@ import { getStack } from "@/app/queries/stack";
 import { getCurrentUser } from "@/app/queries/user";
 import { getBlurData } from "@/app/utils/getBlurData";
 import { getTotalNumberOfDeckQuestions } from "@/app/utils/question";
+import { ESpecialStack } from "@prisma/client";
 import { notFound } from "next/navigation";
 
 type PageProps = {
@@ -14,6 +15,11 @@ type PageProps = {
 const StackPage = async ({ params: { id } }: PageProps) => {
   const [stack, user] = await Promise.all([getStack(+id), getCurrentUser()]);
 
+  const FF_ASK = process.env.NEXT_PUBLIC_FF_ASK === "true";
+
+  if (!FF_ASK && stack?.specialId == ESpecialStack.CommunityAsk)
+    return notFound();
+
   if (!stack || (stack.isActive === false && stack?.isVisible === false))
     return notFound();
 
@@ -21,7 +27,7 @@ const StackPage = async ({ params: { id } }: PageProps) => {
 
   const totalNumberOfCards = getTotalNumberOfDeckQuestions(deckQuestions);
 
-  const blurData = await getBlurData(stack.image);
+  const blurData = stack.image ? await getBlurData(stack.image) : undefined;
 
   return (
     <Stack
