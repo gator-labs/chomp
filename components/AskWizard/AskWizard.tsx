bd65ce0d0bc2f9295feace6a85358fb31deb843a
Shaun Amott <shaun@inerd.com>
2025-04-09 21:34:52 +0200

    Ask wizard: first batch of updates.

---
+++ components/AskWizard/AskWizard.tsx
@@ -0,0 +1,62 @@
+"use client";
+
+import AskForm from "@/app/components/AskForm/AskForm";
+import { AskQuestionType } from "@/components/AskWizard/AskQuestionType";
+import { QuestionType } from "@prisma/client";
+import React, { useState } from "react";
+
+import { AskWizardHeader } from "./AskWizardHeader";
+
+enum Page {
+  QuestionType,
+  QuestionForm,
+  Confirmation,
+}
+
+const POINTS_PER_QUESTION = 69;
+
+export function AskWizard() {
+  const [questionType, setQuestionType] = useState<QuestionType>(
+    QuestionType.BinaryQuestion,
+  );
+  const [page, setPage] = useState<Page>(Page.QuestionType);
+
+  const handleSelectQuestionType = (type: QuestionType) => {
+    setQuestionType(type);
+    setPage(Page.QuestionForm);
+  };
+
+  const handleSetPage = (page: Page) => {
+    setPage(page);
+  };
+
+  return (
+    <>
+      {page === Page.QuestionType && (
+        <div className="flex flex-col gap-3 h-[calc(100vh_-_12em)]">
+          <AskQuestionType
+            type={QuestionType.MultiChoice}
+            points={POINTS_PER_QUESTION}
+            onClick={() => handleSelectQuestionType(QuestionType.MultiChoice)}
+          />
+          <AskQuestionType
+            type={QuestionType.BinaryQuestion}
+            points={POINTS_PER_QUESTION}
+            onClick={() =>
+              handleSelectQuestionType(QuestionType.BinaryQuestion)
+            }
+          />
+        </div>
+      )}
+      {page === Page.QuestionForm && (
+        <div>
+          <AskWizardHeader
+            questionType={questionType}
+            onSetPage={handleSetPage}
+          />
+          <AskForm questionType={questionType} onSetPage={handleSetPage} />
+        </div>
+      )}
+    </>
+  );
+}
