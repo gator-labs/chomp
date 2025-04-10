4835fad00d23eabd994239de07fed1abf9bd065d
Shaun Amott <shaun@inerd.com>
2025-04-10 22:23:22 +0200

    Add gator background; hide overflow.

--- components/AskWizard/AskQuestionPreview.tsx
+++ components/AskWizard/AskQuestionPreview.tsx
@@ -1,4 +1,6 @@
 import { getAlphaIdentifier } from "@/app/utils/question";
+import gatorHeadImage from "@/public/images/gator-head.png";
+import Image from "next/image";
 
 import { AskImagePreview } from "./AskImagePreview";
 
@@ -19,7 +21,7 @@ function MultiChoiceOption({ option, index }: OptionProps) {
       <div className="bg-gray-600 text-base font-bold rounded-lg flex gap-2 items-center aspect-square justify-center p-2">
         {getAlphaIdentifier(index)}
       </div>
-      <div className="border border-gray-500 rounded-lg text-sm font-medium align-middle w-full p-4">
+      <div className="border border-gray-500 rounded-lg text-sm font-medium align-middle w-full p-4 truncate">
         {option}
       </div>
     </div>
@@ -29,7 +31,7 @@ function MultiChoiceOption({ option, index }: OptionProps) {
 function BinaryOption({ option }: OptionProps) {
   return (
     <div className="flex gap-2">
-      <div className="bg-purple-500 rounded-lg text-base font-black align-middle w-full p-4">
+      <div className="bg-purple-500 rounded-lg text-base font-black align-middle w-full p-4 truncate">
         {option}
       </div>
     </div>
@@ -46,8 +48,15 @@ export function AskQuestionPreview({
   return (
     <div className="flex flex-col gap-2">
       <div className="block mb-1 text-base font-medium">Preview</div>
-      <div className="bg-purple-500 rounded-xl flex flex-col py-3 px-5">
-        <div className="font-bold text-xl py-2">{title}</div>
+      <div className="bg-purple-500 rounded-xl flex flex-col py-3 px-5 relative overflow-hidden">
+        <Image
+          src={gatorHeadImage}
+          alt="gator-head"
+          className="absolute bottom-0 w-[241px] h-[93px] z-10"
+        />
+        <div className="font-bold text-xl py-2 wrap overflow-hidden">
+          {title}
+        </div>
         <div className="flex justify-end">
           <AskImagePreview imageUrl={imageUrl} />
         </div>
