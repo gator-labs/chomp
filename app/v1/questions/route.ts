d441fdf41dc353c9e9e71bce285272c17437004f
Marvin Arnold <marvin@geeky.rocks>
2025-04-22 16:52:14 -0500

    debug: log request

--- app/v1/questions/route.ts
+++ app/v1/questions/route.ts
@@ -1,5 +1,12 @@
-export async function POST() {
+import { type NextRequest } from "next/server";
 
+export async function POST(request: NextRequest) {
+  console.error("create-question")
+  console.error(request)
+  console.error(request.headers)
+
+  // const body = await request.json();
+  // console.log("got body", body);
   return Response.json({
     message: "Hello, world!",
   });
