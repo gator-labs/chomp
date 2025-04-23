ea37368dd7794f63c99503f26368292d2a6ed836
Psypersky <alkazar.r@gmail.com>
2025-04-23 10:45:21 -0600

    make mystery box campaigns hideable

--- lib/mysteryBox/createCampaignMysteryBox.ts
+++ lib/mysteryBox/createCampaignMysteryBox.ts
@@ -13,18 +13,14 @@ import "server-only";
 import { getBonkAddress } from "../env-vars";
 
 /**
- * @description Validate campaign details. Create new mystery box if doesn't exist else return the existing mb id for new and unclaimed box.
- * @param {string} userId - The unique identifier of the user
- * @param {string} [campaignBoxId] - Optional. The identifier of the campaign box
- * @returns {Promise<string[]>} A promise that resolves to an array of string IDs representing mystery box identifiers
+ * Validate campaign details.
+ * Create new mystery box if doesn't exist else return the existing mb id for new and unclaimed box.
  */
-
 export const createCampaignMysteryBox = async (
   userId: string,
   campaignBoxId?: string,
-) => {
+): Promise<string[] | null> => {
   const userWallet = await prisma.wallet.findFirst({ where: { userId } });
-
   if (!userWallet) return null;
 
   if (!campaignBoxId) {
