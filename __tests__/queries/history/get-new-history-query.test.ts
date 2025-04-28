cf2f1d1cae2a48ae45fcd9b4e8abf7fbec717464
Shaun Amott <shaun@inerd.com>
2025-04-28 16:16:25 +0200

    Add tests for answer header counts.

--- __tests__/queries/history/get-new-history-query.test.ts
+++ __tests__/queries/history/get-new-history-query.test.ts
@@ -1,5 +1,8 @@
 import { deleteDeck } from "@/app/actions/deck/deck";
-import { getNewHistoryQuery } from "@/app/queries/history";
+import {
+  getHistoryHeadersData,
+  getNewHistoryQuery,
+} from "@/app/queries/history";
 import prisma from "@/app/services/prisma";
 import { generateUsers } from "@/scripts/utils";
 import { QuestionType } from "@prisma/client";
@@ -168,6 +171,19 @@ describe("getNewHistoryQuery", () => {
         indicatorType: "unseen",
       }),
     );
+
+    const counts = await getHistoryHeadersData("mock-id", deckId);
+
+    expect(counts).toEqual(
+      expect.objectContaining({
+        correctCount: 0,
+        incorrectCount: 0,
+        unansweredCount: 0,
+        unrevealedCount: 0,
+        unseenCount: 1,
+        incompleteCount: 0,
+      }),
+    );
   });
 
   it("should return correct indicator type", async () => {
@@ -205,6 +221,19 @@ describe("getNewHistoryQuery", () => {
         indicatorType: "correct",
       }),
     );
+
+    const counts = await getHistoryHeadersData(answer?.userId, deckId);
+
+    expect(counts).toEqual(
+      expect.objectContaining({
+        correctCount: 1,
+        incorrectCount: 0,
+        unansweredCount: 0,
+        unrevealedCount: 0,
+        unseenCount: 0,
+        incompleteCount: 0,
+      }),
+    );
   });
 
   it("should return incorrect indicator type", async () => {
@@ -222,6 +251,19 @@ describe("getNewHistoryQuery", () => {
       expect.objectContaining({
         id: questionId,
         indicatorType: "incorrect",
+      }),
+    );
+
+    const counts = await getHistoryHeadersData(answer?.userId, deckId);
+
+    expect(counts).toEqual(
+      expect.objectContaining({
+        correctCount: 0,
+        incorrectCount: 1,
+        unansweredCount: 0,
+        unrevealedCount: 0,
+        unseenCount: 0,
+        incompleteCount: 0,
       }),
     );
   });
@@ -256,6 +298,19 @@ describe("getNewHistoryQuery", () => {
         indicatorType: "incomplete",
       }),
     );
+
+    const counts = await getHistoryHeadersData(answer?.userId, deckId);
+
+    expect(counts).toEqual(
+      expect.objectContaining({
+        correctCount: 0,
+        incorrectCount: 0,
+        unansweredCount: 0,
+        unrevealedCount: 0,
+        unseenCount: 0,
+        incompleteCount: 1,
+      }),
+    );
   });
 
   it("should return incomplete indicator type", async () => {
@@ -288,6 +343,19 @@ describe("getNewHistoryQuery", () => {
         indicatorType: "unanswered",
       }),
     );
+
+    const counts = await getHistoryHeadersData(answer?.userId, deckId);
+
+    expect(counts).toEqual(
+      expect.objectContaining({
+        correctCount: 0,
+        incorrectCount: 0,
+        unansweredCount: 1,
+        unrevealedCount: 0,
+        unseenCount: 0,
+        incompleteCount: 0,
+      }),
+    );
   });
 
   it("should return unseen indicator type", async () => {
@@ -317,6 +385,19 @@ describe("getNewHistoryQuery", () => {
       expect.objectContaining({
         id: questionId,
         indicatorType: "unseen",
+      }),
+    );
+
+    const counts = await getHistoryHeadersData(userId, deckId);
+
+    expect(counts).toEqual(
+      expect.objectContaining({
+        correctCount: 0,
+        incorrectCount: 0,
+        unansweredCount: 0,
+        unrevealedCount: 0,
+        unseenCount: 1,
+        incompleteCount: 0,
       }),
     );
   });
