f2222604188c916f84aeb596a205c876630721bd
Marvin Arnold <marvin@geeky.rocks>
2025-04-30 14:07:16 -0500

    feat: move API directory to v1

--- app/v1/questions/route.ts
+++ app/v1/questions/route.ts
@@ -2,17 +2,20 @@ import { questionSchema } from "@/app/schemas/v1/question";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(request: NextRequest) {
-  console.log("got request", request);
+  console.error("got request", request);
   const data = await request.json();
-  console.log("got data", data);
+  console.error("got data", data);
   // parse backend-secret and source headers
   const backendSecret = request.headers.get("backend-secret");
   if (backendSecret !== process.env.BACKEND_SECRET) {
     return Response.json({ error: "Unauthorized" }, { status: 401 });
   }
 
+  // source corresponds to Zuplo -> Services -> API Key Service -> select consumer -> metadata -> source
+  // containts values like 'melee' or 'marvin'
   const source = request.headers.get("source");
-  console.log("got source", source);
+  console.error("got source", source);
+  
   try {
     const validation = questionSchema.safeParse(data);
     console.log(validation.error);
