625381ebbfca568f1f3389c7cd579de88cdbe50f
Shaun Amott <shaun@inerd.com>
2025-05-05 20:35:04 +0200

    Implement answer question endpoint.

---
+++ lib/v1/answerQuestion.ts
@@ -0,0 +1,90 @@
+import prisma from "@/app/services/prisma";
+import "server-only";
+import { v4 as uuidv4 } from "uuid";
+
+import {
+  ApiAnswerInvalidError,
+  ApiOptionInvalidError,
+  ApiQuestionInactiveError,
+  ApiQuestionInvalidError,
+} from "../error";
+
+export async function answerQuestion(
+  userId: string,
+  questionId: string,
+  source: string,
+  firstOrderOptionId: string,
+  secondOrderOptionId: string,
+  secondOrderEstimate: number,
+): Promise<string> {
+  const now = new Date();
+
+  const existing = await prisma.questionAnswer.findFirst({
+    where: {
+      userId,
+      questionOption: {
+        question: {
+          uuid: questionId,
+        },
+      },
+    },
+  });
+
+  if (existing) {
+    throw new ApiAnswerInvalidError(
+      "User already submitted an answer for this question",
+    );
+  }
+
+  const options = await prisma.questionOption.findMany({
+    where: {
+      question: {
+        uuid: questionId,
+        source,
+      },
+    },
+    include: {
+      question: true,
+    },
+  });
+
+  if (options.length === 0) {
+    throw new ApiQuestionInvalidError("Question not found or not answerable");
+  }
+
+  const question = options[0].question;
+
+  if (!question.activeFromDate || new Date(question.activeFromDate) > now) {
+    throw new ApiQuestionInactiveError("Question is not answerable yet");
+  }
+
+  const firstOrder = options.find(
+    (option) => option.uuid === firstOrderOptionId,
+  );
+  const secondOrder = options.find(
+    (option) => option.uuid === secondOrderOptionId,
+  );
+
+  if (!firstOrder)
+    throw new ApiOptionInvalidError("First order option not valid");
+
+  if (!secondOrder)
+    throw new ApiOptionInvalidError("Second order option not valid");
+
+  const uuid = uuidv4();
+
+  const answers = options.map((option) => ({
+    uuid,
+    userId,
+    questionOptionId: option.id,
+    selected: option.uuid === firstOrderOptionId,
+    percentage:
+      option.uuid === secondOrderOptionId ? secondOrderEstimate : null,
+  }));
+
+  const res = await prisma.questionAnswer.createManyAndReturn({
+    data: answers,
+  });
+
+  return res[0].uuid;
+}
