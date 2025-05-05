53d9de1a07ba7a36276e65af0d9536064f77a1e5
Yash Garg <yash431garg@gmail.com>
2025-05-05 16:55:16 +0530

    fix(migration): missing semicolon

--- prisma/migrations/20250505061740_add_question_active_from_date_and_question_option_uuid_coulmn/migration.sql
+++ prisma/migrations/20250505061740_add_question_active_from_date_and_question_option_uuid_coulmn/migration.sql
@@ -6,7 +6,7 @@ ALTER TABLE "QuestionOption" ADD COLUMN "uuid" UUID;
 UPDATE "QuestionOption" SET "uuid" = gen_random_uuid() WHERE "uuid" IS NULL;
 
 -- AlterTable
-ALTER TABLE "QuestionOption" ALTER COLUMN "uuid" SET NOT NULL
+ALTER TABLE "QuestionOption" ALTER COLUMN "uuid" SET NOT NULL;
 
 -- CreateIndex
 CREATE UNIQUE INDEX "QuestionOption_uuid_key" ON "QuestionOption"("uuid");
