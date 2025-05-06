9c69d505d8ae730c686f8a87dd09c6eeeeb66ba3
Shaun Amott <shaun@inerd.com>
2025-05-06 03:13:51 +0200

    Update migration to fill in UUIDs for QuestionAnswers (grouped by Question).

--- prisma/migrations/20250506003748_add_questionanswer_uuid_column/migration.sql
+++ prisma/migrations/20250506003748_add_questionanswer_uuid_column/migration.sql
@@ -1,8 +1,23 @@
-/*
-  Warnings:
+ALTER TABLE "QuestionAnswer" ADD COLUMN "uuid" UUID;
 
-  - The required column `uuid` was added to the `QuestionAnswer` table with a prisma-level default value. This is not possible if the table is not empty. Please add this column as optional, then populate it before making it required.
+-- Updates QuestionAnswers to have a single UUID
+-- that is shared among the answers for a given
+-- question.
 
-*/
--- AlterTable
-ALTER TABLE "QuestionAnswer" ADD COLUMN     "uuid" UUID NOT NULL;
+WITH "quuids" AS (
+  SELECT "questionId", qo.id AS "questionOptionId", "quuid"
+  FROM "QuestionOption" qo
+  LEFT JOIN (
+    SELECT id, gen_random_uuid() as quuid
+    FROM "Question"
+  ) q
+  ON qo."questionId" = q.id
+)
+UPDATE
+  "QuestionAnswer" qa
+  SET uuid = quuids.quuid
+  FROM quuids
+  WHERE quuids."questionOptionId" = qa."questionOptionId"
+  AND qa.uuid IS NULL;
+
+ALTER TABLE "QuestionAnswer" ALTER COLUMN "uuid" SET NOT NULL;
