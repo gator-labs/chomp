517ed4b415c19ad1cd3d06da169ad4f403cabb78
Shaun Amott <shaun@inerd.com>
2025-05-26 18:11:40 +0200

    Add missing file from previous commit.

--- lib/ask/getCommunityAskList.ts
+++ lib/ask/getCommunityAskList.ts
@@ -1,7 +1,7 @@
 "server-only";
 
 import prisma from "@/app/services/prisma";
-import { CommunityAskFilter } from "@/types/ask";
+import { CommunityAskFilter, CommunityAskSortBy, SortOrder } from "@/types/ask";
 import {
   DeckQuestion,
   Question,
@@ -17,8 +17,22 @@ export type CommunityAskQuestion = Question & {
   deckQuestions?: DeckQuestion[];
 };
 
+const getSortQuery = (
+  _filter: CommunityAskFilter,
+  sortBy: CommunityAskSortBy,
+  sortOrder: SortOrder,
+) => {
+  if (sortBy === "userId") {
+    return { user: { id: sortOrder } };
+  }
+
+  return { createdAt: sortOrder };
+};
+
 export async function getCommunityAskList(
   filter: CommunityAskFilter,
+  sortBy: CommunityAskSortBy,
+  sortOrder: SortOrder,
 ): Promise<CommunityAskQuestion[]> {
   const filterQuery =
     filter === "pending"
@@ -38,6 +52,8 @@ export async function getCommunityAskList(
             isArchived: true,
           };
 
+  const sortQuery = getSortQuery(filter, sortBy, sortOrder);
+
   const askList = await prisma.question.findMany({
     where: {
       isSubmittedByUser: true,
@@ -53,9 +69,7 @@ export async function getCommunityAskList(
       questionOptions: true,
       ...(filter === "accepted" ? { deckQuestions: true } : null),
     },
-    orderBy: {
-      createdAt: "desc",
-    },
+    orderBy: sortQuery,
   });
 
   return askList.map((question) => ({
