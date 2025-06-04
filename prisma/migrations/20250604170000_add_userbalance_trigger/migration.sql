4bf6fdd5588c6f9bc2246f543f1dda3d920c4611
Shaun Amott <shaun@inerd.com>
2025-06-04 15:09:51 +0200

    Add table prefix.

--- prisma/migrations/20250604170000_add_userbalance_trigger/migration.sql
+++ prisma/migrations/20250604170000_add_userbalance_trigger/migration.sql
@@ -2,33 +2,33 @@ CREATE OR REPLACE FUNCTION sync_user_balance()
 RETURNS trigger AS $$
 BEGIN
   IF (TG_OP = 'INSERT') THEN
-    INSERT INTO "UserBalance" ("userId", asset, balance)
+    INSERT INTO public."UserBalance" ("userId", asset, balance)
     VALUES (NEW."userId", NEW.asset, NEW.change)
     ON CONFLICT ("userId", asset)
-    DO UPDATE SET balance = "UserBalance".balance + EXCLUDED.balance;
+    DO UPDATE SET balance = public."UserBalance".balance + EXCLUDED.balance;
 
   ELSIF (TG_OP = 'DELETE') THEN
-    UPDATE "UserBalance"
+    UPDATE public."UserBalance"
     SET balance = balance - OLD.change
     WHERE "userId" = OLD."userId" AND asset = OLD.asset;
 
   ELSIF (TG_OP = 'UPDATE') THEN
     IF (OLD."userId" = NEW."userId" AND OLD.asset = NEW.asset) THEN
       -- Straight update to FATL entry: need to sync the net change
-      UPDATE "UserBalance"
+      UPDATE public."UserBalance"
       SET balance = balance + (NEW.change - OLD.change)
       WHERE "userId" = NEW."userId" AND asset = NEW.asset;
     ELSE
       -- FATL entry userId or asset changing: both records
       -- must be updated in this case
-      UPDATE "UserBalance"
+      UPDATE public."UserBalance"
       SET balance = balance - OLD.change
       WHERE "userId" = OLD."userId" AND asset = OLD.asset;
 
-      INSERT INTO "UserBalance" ("userId", asset, balance)
+      INSERT INTO public."UserBalance" ("userId", asset, balance)
       VALUES (NEW."userId", NEW.asset, NEW.change)
       ON CONFLICT ("userId", asset)
-      DO UPDATE SET balance = "UserBalance".balance + EXCLUDED.balance;
+      DO UPDATE SET balance = public."UserBalance".balance + EXCLUDED.balance;
     END IF;
   END IF;
 
@@ -37,5 +37,5 @@ END;
 $$ LANGUAGE plpgsql;
 
 CREATE TRIGGER trigger_sync_user_balance
-AFTER INSERT OR UPDATE OR DELETE ON "FungibleAssetTransactionLog"
+AFTER INSERT OR UPDATE OR DELETE ON public."FungibleAssetTransactionLog"
 FOR EACH ROW EXECUTE FUNCTION sync_user_balance();
