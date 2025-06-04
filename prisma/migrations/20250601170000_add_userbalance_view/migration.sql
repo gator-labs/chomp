e15325602c7560f800b546f4bab5fee4822d2af3
Shaun Amott <shaun@inerd.com>
2025-06-04 14:52:48 +0200

    Switch from view approach to synced table for user balances.

--- prisma/migrations/20250601170000_add_userbalance_view/migration.sql
+++
@@ -1,9 +0,0 @@
-CREATE MATERIALIZED VIEW "UserBalance" AS
-SELECT
-    SUM("change") AS "balance",
-    "asset",
-    "userId"
-    FROM "FungibleAssetTransactionLog"
-    GROUP BY ("userId", "asset");
-
-CREATE INDEX "UserBalance_userId_asset" ON "UserBalance" ("userId", "asset");
