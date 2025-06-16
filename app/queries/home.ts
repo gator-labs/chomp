c6f6897f32a6201e661939c0cc7b7134f4c553f3
Shaun Amott <shaun@inerd.com>
2025-06-17 01:32:30 +0200

    Integrate streak extensions into calculation.
    - Extensions contribute to user streaks.
    - Add quotes around table names to properly preserve case.

--- app/queries/home.ts
+++ app/queries/home.ts
@@ -236,51 +236,62 @@ export async function getUsersLatestStreak(): Promise<number> {
 
 async function queryUsersLatestStreak(userId: string): Promise<number> {
   const streaks: Streak[] = await prisma.$queryRaw`
-  WITH userActivity AS (
-    SELECT DISTINCT DATE("createdAt") AS activityDate
+  WITH "userActivity" AS (
+    SELECT DISTINCT DATE("createdAt") AS "activityDate",
+    1 AS "streakValue"
     FROM public."ChompResult"
     WHERE "userId" = ${userId}
     UNION
-    SELECT DISTINCT DATE("createdAt") AS activityDate
+    SELECT DISTINCT DATE("createdAt") AS "activityDate",
+    1 AS "streakValue"
     FROM public."QuestionAnswer" qa
     WHERE "userId" = ${userId}
     AND qa."status" = 'Submitted'
     UNION
-    SELECT DISTINCT DATE("createdAt") AS activityDate
+    SELECT DISTINCT DATE("createdAt") AS "activityDate",
+    1 AS "streakValue"
     FROM public."FungibleAssetTransactionLog" fatl
     WHERE "userId" = ${userId}
     AND fatl."asset" = 'Credit'
     AND fatl."type" = 'CreditPurchase'
     UNION
-    SELECT DISTINCT DATE("createdAt") AS activityDate
+    SELECT DISTINCT DATE("createdAt") AS "activityDate",
+    1 AS "streakValue"
     FROM public."MysteryBox" mbox
     WHERE "userId" = ${userId}
     UNION
-    SELECT DISTINCT DATE("createdAt") AS activityDate
+    SELECT DISTINCT DATE("createdAt") AS "activityDate",
+    1 AS "streakValue"
     FROM public."Question" mbox
     WHERE "createdByUserId" = ${userId}
+    UNION
+    SELECT "activityDate", "streakValue"
+    FROM public."StreakExtension" se
+    WHERE "userId" IS NULL OR "userId" = ${userId}
   ),
-  consecutiveDays AS (
+  "consecutiveDays" AS (
     SELECT
-      activityDate,
-      LAG(activityDate) OVER (ORDER BY activityDate) AS previousDate
-    FROM userActivity
+      "activityDate",
+      LAG("activityDate") OVER (ORDER BY "activityDate") AS "previousDate",
+      "streakValue"
+    FROM "userActivity"
   ),
   "streakGroups" AS (
     SELECT
-      activityDate,
-      SUM(CASE WHEN activityDate = previousDate + INTERVAL '1 day' THEN 0 ELSE 1 END)
-      OVER (ORDER BY activityDate) AS "streakGroup"
-    FROM consecutiveDays
+      "activityDate",
+      SUM(CASE WHEN "activityDate" = "previousDate" + INTERVAL '1 day' THEN 0 ELSE 1 END)
+      OVER (ORDER BY "activityDate") AS "streakGroup",
+      "streakValue"
+    FROM "consecutiveDays"
   )
   SELECT
-    MIN(activityDate) AS "streakStartDate",
-    MAX(activityDate) AS "streakEndDate",
-    COUNT(*) AS "streakLength"
+    MIN("activityDate") AS "streakStartDate",
+    MAX("activityDate") AS "streakEndDate",
+    SUM("streakValue") AS "streakLength"
   FROM "streakGroups"
   GROUP BY "streakGroup"
-  HAVING MAX(activityDate) IN (CURRENT_DATE, CURRENT_DATE - INTERVAL '1 day')
-  ORDER BY MAX(activityDate) DESC
+  HAVING MAX("activityDate") IN (CURRENT_DATE, CURRENT_DATE - INTERVAL '1 day')
+  ORDER BY MAX("activityDate") DESC
   LIMIT 1
   `;
 
