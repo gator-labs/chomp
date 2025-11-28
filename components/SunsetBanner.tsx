82e046a2a09a228060945d52eeefc08ff7e67cc0
Shaun Amott <shaun@inerd.com>
2025-11-28 03:19:34 +0100

    Update leaderboard link in banner.

--- components/SunsetBanner.tsx
+++ components/SunsetBanner.tsx
@@ -1,19 +1,29 @@
-import Link from "next/link";
 import classNames from "classnames";
+import Link from "next/link";
 
 export type SunsetBannerProps = {
   className?: string;
-}
+};
 
 export function SunsetBanner({ className }: SunsetBannerProps) {
   return (
     <div className={classNames("flex justify-center w-full", className)}>
       <div className="max-w-lg bg-[#ED6A5A] p-5">
-        <div className="font-bold font-black text-xs sm:text-lg">CHOMPY IS MOVING</div>
+        <div className="font-bold font-black text-xs sm:text-lg">
+          CHOMPY IS MOVING
+        </div>
         <div className="text-xs sm:text-sm">
-          This version of CHOMP will go offline on December 19th 11:59pm UTC.
-          Be in Top 100 All Time Points <Link href="/Leaderboard" className="underline">Leaderboard</Link> by December 14th
-          11:59pm to claim a gift from Chompy! Read more about it <Link href="/" className="underline">here</Link>.
+          This version of CHOMP will go offline on December 19th 11:59pm UTC. Be
+          in Top 100 All Time Points{" "}
+          <Link href="/application/leaderboard" className="underline">
+            Leaderboard
+          </Link>{" "}
+          by December 14th 11:59pm to claim a gift from Chompy! Read more about
+          it{" "}
+          <Link href="/" className="underline">
+            here
+          </Link>
+          .
         </div>
       </div>
     </div>
